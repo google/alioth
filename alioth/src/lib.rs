407c80af95a06c7992434adc4c0112e366401368
Changyuan Lyu <changyuanl@google.com>
2024-04-24 20:56:09 -0700

    feat!: remove backtrace from Error enums
    It is an inevitable regression if we want to build
    with stable rust :(
    
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- alioth/src/lib.rs
+++ alioth/src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(error_generic_member_access)]
-
 pub mod acpi;
 pub mod action;
 pub mod arch;
