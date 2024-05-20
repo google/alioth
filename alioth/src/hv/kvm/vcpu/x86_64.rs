4162b32ff2e1d6827ea20ea8df1631b2435d56cf
Changyuan Lyu <changyuanl@google.com>
2024-05-19 23:26:54 -0700

    fix(kvm): use old KVM_GET_SREGS/KVM_SET_SREGS
    For now we do not need the new features of
    KVM_GET_SREGS2/KVM_SET_SREGS2. Use the old ioctls for better
    compatibility.
    
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- alioth/src/hv/kvm/vcpu/x86_64.rs
+++ alioth/src/hv/kvm/vcpu/x86_64.rs
@@ -15,25 +15,125 @@
 use crate::arch::reg::SegAccess;
 use crate::hv::arch::{Cpuid, DtReg, DtRegVal, Reg, SReg, SegReg, SegRegVal};
 use crate::hv::kvm::bindings::{
-    KvmCpuid2, KvmCpuid2Flag, KvmCpuidEntry2, KvmRegs, KvmSregs2, KVM_MAX_CPUID_ENTRIES,
+    KvmCpuid2, KvmCpuid2Flag, KvmCpuidEntry2, KvmRegs, KVM_MAX_CPUID_ENTRIES,
 };
 use crate::hv::kvm::ioctls::{
-    kvm_get_regs, kvm_get_sregs2, kvm_set_cpuid2, kvm_set_regs, kvm_set_sregs2,
+    kvm_get_regs, kvm_get_sregs, kvm_get_sregs2, kvm_set_cpuid2, kvm_set_regs, kvm_set_sregs,
+    kvm_set_sregs2,
 };
 use crate::hv::kvm::vcpu::KvmVcpu;
 use crate::hv::{Error, Result};
 
+macro_rules! set_kvm_sreg {
+    ($kvm_sregs:ident, $sreg:ident, $val:expr) => {
+        match $sreg {
+            SReg::Cr0 => $kvm_sregs.cr0 = $val,
+            SReg::Cr2 => $kvm_sregs.cr2 = $val,
+            SReg::Cr3 => $kvm_sregs.cr3 = $val,
+            SReg::Cr4 => $kvm_sregs.cr4 = $val,
+            SReg::Cr8 => $kvm_sregs.cr8 = $val,
+            SReg::Efer => $kvm_sregs.efer = $val,
+            SReg::ApicBase => $kvm_sregs.apic_base = $val,
+        }
+    };
+}
+
+macro_rules! set_kvm_dt_reg {
+    ($kvm_sregs:ident, $dt_reg:ident, $val:expr) => {
+        let target = match $dt_reg {
+            DtReg::Idtr => &mut $kvm_sregs.idt,
+            DtReg::Gdtr => &mut $kvm_sregs.gdt,
+        };
+        target.limit = $val.limit;
+        target.base = $val.base;
+    };
+}
+
+macro_rules! set_kvm_seg_reg {
+    ($kvm_sregs:ident, $seg_reg:ident, $val:expr) => {
+        let target = match $seg_reg {
+            SegReg::Cs => &mut $kvm_sregs.cs,
+            SegReg::Ds => &mut $kvm_sregs.ds,
+            SegReg::Es => &mut $kvm_sregs.es,
+            SegReg::Fs => &mut $kvm_sregs.fs,
+            SegReg::Gs => &mut $kvm_sregs.gs,
+            SegReg::Ss => &mut $kvm_sregs.ss,
+            SegReg::Tr => &mut $kvm_sregs.tr,
+            SegReg::Ldtr => &mut $kvm_sregs.ldt,
+        };
+        target.selector = $val.selector;
+        target.base = $val.base;
+        target.limit = $val.limit;
+        target.type_ = $val.access.seg_type() as u8;
+        target.s = $val.access.s_code_data() as u8;
+        target.dpl = $val.access.priv_level() as u8;
+        target.present = $val.access.present() as u8;
+        target.avl = $val.access.available() as u8;
+        target.db = $val.access.db_size_32() as u8;
+        target.g = $val.access.granularity() as u8;
+        target.l = $val.access.l_64bit() as u8;
+        target.unusable = $val.access.unusable() as u8;
+    };
+}
+
+macro_rules! get_kvm_sreg {
+    ($kvm_sregs:ident, $sreg:ident) => {
+        match $sreg {
+            SReg::Cr0 => $kvm_sregs.cr0,
+            SReg::Cr2 => $kvm_sregs.cr2,
+            SReg::Cr3 => $kvm_sregs.cr3,
+            SReg::Cr4 => $kvm_sregs.cr4,
+            SReg::Cr8 => $kvm_sregs.cr8,
+            SReg::Efer => $kvm_sregs.efer,
+            SReg::ApicBase => $kvm_sregs.apic_base,
+        }
+    };
+}
+
+macro_rules! get_kvm_dt_reg {
+    ($kvm_sregs:ident, $dt_reg:ident) => {{
+        let target = match $dt_reg {
+            DtReg::Idtr => $kvm_sregs.idt,
+            DtReg::Gdtr => $kvm_sregs.gdt,
+        };
+        DtRegVal {
+            limit: target.limit,
+            base: target.base,
+        }
+    }};
+}
+
+macro_rules! get_kvm_seg_reg {
+    ($kvm_sregs:ident, $seg_reg:ident) => {{
+        let kvm_segment = match $seg_reg {
+            SegReg::Cs => $kvm_sregs.cs,
+            SegReg::Ds => $kvm_sregs.ds,
+            SegReg::Es => $kvm_sregs.es,
+            SegReg::Fs => $kvm_sregs.fs,
+            SegReg::Gs => $kvm_sregs.gs,
+            SegReg::Ss => $kvm_sregs.ss,
+            SegReg::Tr => $kvm_sregs.tr,
+            SegReg::Ldtr => $kvm_sregs.ldt,
+        };
+        let access = (kvm_segment.unusable as u32) << 16
+            | (kvm_segment.g as u32) << 15
+            | (kvm_segment.db as u32) << 14
+            | (kvm_segment.l as u32) << 13
+            | (kvm_segment.avl as u32) << 12
+            | (kvm_segment.present as u32) << 7
+            | (kvm_segment.dpl as u32) << 5
+            | (kvm_segment.s as u32) << 4
+            | (kvm_segment.type_ as u32);
+        SegRegVal {
+            selector: kvm_segment.selector,
+            base: kvm_segment.base,
+            limit: kvm_segment.limit,
+            access: SegAccess(access),
+        }
+    }};
+}
+
 impl KvmVcpu {
-    fn get_kvm_sregs2(&self) -> Result<KvmSregs2> {
-        let kvm_sregs2 = unsafe { kvm_get_sregs2(&self.fd) }?;
-        Ok(kvm_sregs2)
-    }
-
-    fn set_kvm_sregs2(&self, kvm_sregs2: &KvmSregs2) -> Result<()> {
-        unsafe { kvm_set_sregs2(&self.fd, kvm_sregs2) }?;
-        Ok(())
-    }
-
     fn get_kvm_regs(&self) -> Result<KvmRegs> {
         let kvm_regs = unsafe { kvm_get_regs(&self.fd) }?;
         Ok(kvm_regs)
@@ -71,60 +171,6 @@ impl KvmVcpu {
         self.set_kvm_regs(&kvm_regs)
     }
 
-    pub fn kvm_set_sregs(
-        &mut self,
-        sregs: &[(SReg, u64)],
-        seg_regs: &[(SegReg, SegRegVal)],
-        dt_regs: &[(DtReg, DtRegVal)],
-    ) -> Result<(), Error> {
-        let mut kvm_sregs2 = self.get_kvm_sregs2()?;
-        for (reg, val) in sregs {
-            match reg {
-                SReg::Cr0 => kvm_sregs2.cr0 = *val,
-                SReg::Cr2 => kvm_sregs2.cr2 = *val,
-                SReg::Cr3 => kvm_sregs2.cr3 = *val,
-                SReg::Cr4 => kvm_sregs2.cr4 = *val,
-                SReg::Cr8 => kvm_sregs2.cr8 = *val,
-                SReg::Efer => kvm_sregs2.efer = *val,
-                SReg::ApicBase => kvm_sregs2.apic_base = *val,
-            }
-        }
-        for (reg, val) in dt_regs {
-            let target = match reg {
-                DtReg::Idtr => &mut kvm_sregs2.idt,
-                DtReg::Gdtr => &mut kvm_sregs2.gdt,
-            };
-            target.limit = val.limit;
-            target.base = val.base;
-        }
-        for (reg, val) in seg_regs {
-            let target = match reg {
-                SegReg::Cs => &mut kvm_sregs2.cs,
-                SegReg::Ds => &mut kvm_sregs2.ds,
-                SegReg::Es => &mut kvm_sregs2.es,
-                SegReg::Fs => &mut kvm_sregs2.fs,
-                SegReg::Gs => &mut kvm_sregs2.gs,
-                SegReg::Ss => &mut kvm_sregs2.ss,
-                SegReg::Tr => &mut kvm_sregs2.tr,
-                SegReg::Ldtr => &mut kvm_sregs2.ldt,
-            };
-            target.selector = val.selector;
-            target.base = val.base;
-            target.limit = val.limit;
-            target.type_ = val.access.seg_type() as u8;
-            target.s = val.access.s_code_data() as u8;
-            target.dpl = val.access.priv_level() as u8;
-            target.present = val.access.present() as u8;
-            target.avl = val.access.available() as u8;
-            target.db = val.access.db_size_32() as u8;
-            target.g = val.access.granularity() as u8;
-            target.l = val.access.l_64bit() as u8;
-            target.unusable = val.access.unusable() as u8;
-        }
-        self.set_kvm_sregs2(&kvm_sregs2)?;
-        Ok(())
-    }
-
     pub fn kvm_get_reg(&self, reg: Reg) -> Result<u64> {
         let kvm_regs = self.get_kvm_regs()?;
         let val = match reg {
@@ -150,59 +196,79 @@ impl KvmVcpu {
         Ok(val)
     }
 
+    pub fn kvm_set_sregs2(
+        &mut self,
+        sregs: &[(SReg, u64)],
+        seg_regs: &[(SegReg, SegRegVal)],
+        dt_regs: &[(DtReg, DtRegVal)],
+    ) -> Result<(), Error> {
+        let mut kvm_sregs2 = unsafe { kvm_get_sregs2(&self.fd) }?;
+        for (reg, val) in sregs {
+            set_kvm_sreg!(kvm_sregs2, reg, *val)
+        }
+        for (reg, val) in dt_regs {
+            set_kvm_dt_reg!(kvm_sregs2, reg, val);
+        }
+        for (reg, val) in seg_regs {
+            set_kvm_seg_reg!(kvm_sregs2, reg, val);
+        }
+        unsafe { kvm_set_sregs2(&self.fd, &kvm_sregs2) }?;
+        Ok(())
+    }
+
+    pub fn kvm_get_dt_reg2(&self, reg: DtReg) -> Result<DtRegVal> {
+        let kvm_sregs2 = unsafe { kvm_get_sregs2(&self.fd) }?;
+        let val = get_kvm_dt_reg!(kvm_sregs2, reg);
+        Ok(val)
+    }
+
+    pub fn kvm_get_seg_reg2(&self, reg: SegReg) -> Result<SegRegVal> {
+        let kvm_sregs2 = unsafe { kvm_get_sregs2(&self.fd) }?;
+        let val = get_kvm_seg_reg!(kvm_sregs2, reg);
+        Ok(val)
+    }
+
+    pub fn kvm_get_sreg2(&self, reg: SReg) -> Result<u64> {
+        let kvm_sregs2 = unsafe { kvm_get_sregs2(&self.fd) }?;
+        let val = get_kvm_sreg!(kvm_sregs2, reg);
+        Ok(val)
+    }
+
+    pub fn kvm_set_sregs(
+        &mut self,
+        sregs: &[(SReg, u64)],
+        seg_regs: &[(SegReg, SegRegVal)],
+        dt_regs: &[(DtReg, DtRegVal)],
+    ) -> Result<(), Error> {
+        let mut kvm_sregs = unsafe { kvm_get_sregs(&self.fd) }?;
+        for (reg, val) in sregs {
+            set_kvm_sreg!(kvm_sregs, reg, *val)
+        }
+        for (reg, val) in dt_regs {
+            set_kvm_dt_reg!(kvm_sregs, reg, val);
+        }
+        for (reg, val) in seg_regs {
+            set_kvm_seg_reg!(kvm_sregs, reg, val);
+        }
+        unsafe { kvm_set_sregs(&self.fd, &kvm_sregs) }?;
+        Ok(())
+    }
+
     pub fn kvm_get_dt_reg(&self, reg: DtReg) -> Result<DtRegVal> {
-        let kvm_sregs2 = self.get_kvm_sregs2()?;
-        let target = match reg {
-            DtReg::Idtr => &kvm_sregs2.idt,
-            DtReg::Gdtr => &kvm_sregs2.gdt,
-        };
-        Ok(DtRegVal {
-            limit: target.limit,
-            base: target.base,
-        })
+        let kvm_sregs = unsafe { kvm_get_sregs(&self.fd) }?;
+        let val = get_kvm_dt_reg!(kvm_sregs, reg);
+        Ok(val)
     }
 
     pub fn kvm_get_seg_reg(&self, reg: SegReg) -> Result<SegRegVal> {
-        let kvm_sregs2 = self.get_kvm_sregs2()?;
-        let kvm_segment = match reg {
-            SegReg::Cs => kvm_sregs2.cs,
-            SegReg::Ds => kvm_sregs2.ds,
-            SegReg::Es => kvm_sregs2.es,
-            SegReg::Fs => kvm_sregs2.fs,
-            SegReg::Gs => kvm_sregs2.gs,
-            SegReg::Ss => kvm_sregs2.ss,
-            SegReg::Tr => kvm_sregs2.tr,
-            SegReg::Ldtr => kvm_sregs2.ldt,
-        };
-        let access = (kvm_segment.unusable as u32) << 16
-            | (kvm_segment.g as u32) << 15
-            | (kvm_segment.db as u32) << 14
-            | (kvm_segment.l as u32) << 13
-            | (kvm_segment.avl as u32) << 12
-            | (kvm_segment.present as u32) << 7
-            | (kvm_segment.dpl as u32) << 5
-            | (kvm_segment.s as u32) << 4
-            | (kvm_segment.type_ as u32);
-        let val = SegRegVal {
-            selector: kvm_segment.selector,
-            base: kvm_segment.base,
-            limit: kvm_segment.limit,
-            access: SegAccess(access),
-        };
+        let kvm_sregs = unsafe { kvm_get_sregs(&self.fd) }?;
+        let val = get_kvm_seg_reg!(kvm_sregs, reg);
         Ok(val)
     }
 
     pub fn kvm_get_sreg(&self, reg: SReg) -> Result<u64> {
-        let kvm_sregs2 = self.get_kvm_sregs2()?;
-        let val = match reg {
-            SReg::Cr0 => kvm_sregs2.cr0,
-            SReg::Cr2 => kvm_sregs2.cr2,
-            SReg::Cr3 => kvm_sregs2.cr3,
-            SReg::Cr4 => kvm_sregs2.cr4,
-            SReg::Cr8 => kvm_sregs2.cr8,
-            SReg::Efer => kvm_sregs2.efer,
-            SReg::ApicBase => kvm_sregs2.apic_base,
-        };
+        let kvm_sregs = unsafe { kvm_get_sregs(&self.fd) }?;
+        let val = get_kvm_sreg!(kvm_sregs, reg);
         Ok(val)
     }
 
