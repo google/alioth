39cdd6d4a225e607d2f3bb31c7cb069d8d802a2a
Changyuan Lyu <changyuanl@google.com>
2024-05-21 00:09:16 -0700

    feat(cli): add a new crate for parsing cli opts
    `serde-aco` is for parsing complex data structures from command line.
    Nested structures can be expressed by multiple key-value pair
    strings.
    
    This crate is inspired by QEMU's `-object` flag.
    
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

---
+++ serde-aco/src/error.rs
@@ -0,0 +1,57 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::fmt::{self, Display};
+
+use serde::{de, ser};
+
+#[derive(Debug)]
+pub enum Error {
+    Message(String),
+    ExpectedMapComma,
+    ExpectedMapEq,
+    IdNotFound,
+    ExpectedInteger,
+    ExpectedBool,
+    Overflow,
+    Eof,
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl de::Error for Error {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: Display,
+    {
+        Error::Message(msg.to_string())
+    }
+}
+
+impl ser::Error for Error {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: Display,
+    {
+        Error::Message(msg.to_string())
+    }
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
