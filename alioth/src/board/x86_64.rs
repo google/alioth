51b02c96a9386958b05cfcf0d37464cfe4a31a12
Changyuan Lyu <changyuanl@google.com>
2024-05-21 22:52:47 -0700

    feat(sev): set up AP registers for SEV-ES guests
    This includes
    
    * parse the firmware blob to get the AP EIP value
    * set up AP registers based on the parsed EIP
    * call sev_launch_update_vmsa before booting CPUs
    
    Ref:
    [1] QEMU hw/i386/pc_sysfw_ovmf.c
    [2] QEMU docs/specs/sev-guest-firmware.rst
    [3] https://github.com/project-oak/oak snp_measurement/src/stage0.rs
    
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- alioth/src/board/x86_64.rs
+++ alioth/src/board/x86_64.rs
@@ -13,13 +13,17 @@
 // limitations under the License.
 
 use std::arch::x86_64::__cpuid;
+use std::mem::size_of;
+use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 
 use parking_lot::Mutex;
+use zerocopy::FromBytes;
 
 use crate::arch::layout::{BIOS_DATA_END, EBDA_END, EBDA_START, MEM_64_START, RAM_32_SIZE};
-use crate::board::{Board, BoardConfig, Result};
-use crate::hv::arch::Cpuid;
+use crate::arch::reg::SegAccess;
+use crate::board::{Board, BoardConfig, Result, VcpuGuard};
+use crate::hv::arch::{Cpuid, Reg, SegReg, SegRegVal};
 use crate::hv::{Coco, Hypervisor, Vcpu, Vm};
 use crate::loader::InitState;
 use crate::mem::mapped::ArcMemPages;
@@ -27,6 +31,7 @@ use crate::mem::{AddrOpt, MemRange, MemRegion, MemRegionEntry, MemRegionType};
 
 pub struct ArchBoard {
     cpuids: Vec<Cpuid>,
+    sev_ap_eip: AtomicU32,
 }
 
 impl ArchBoard {
@@ -47,7 +52,10 @@ impl ArchBoard {
                 }
             }
         }
-        Ok(Self { cpuids })
+        Ok(Self {
+            cpuids,
+            sev_ap_eip: AtomicU32::new(0),
+        })
     }
 }
 
@@ -57,7 +65,11 @@ where
 {
     pub fn setup_firmware(&self, fw: &mut ArcMemPages) -> Result<()> {
         match &self.config.coco {
-            Some(Coco::AmdSev { .. }) => {
+            Some(Coco::AmdSev { policy }) => {
+                if policy.es() {
+                    let ap_eip = get_ap_eip_from_fw(fw.as_slice()).unwrap();
+                    self.arch.sev_ap_eip.store(ap_eip, Ordering::Release);
+                }
                 self.memory.ram_bus().register_encrypted_pages(&fw)?;
                 self.vm.sev_launch_update_data(fw.as_slice_mut())?;
             }
@@ -65,6 +77,36 @@ where
         }
         Ok(())
     }
+
+    pub fn init_ap(&self, id: u32, vcpu: &mut V::Vcpu, vcpus: &VcpuGuard) -> Result<()> {
+        let Some(Coco::AmdSev { policy }) = &self.config.coco else {
+            return Ok(());
+        };
+        if !policy.es() {
+            return Ok(());
+        }
+        self.sync_vcpus(vcpus);
+        if id == 0 {
+            return Ok(());
+        }
+        let eip = self.arch.sev_ap_eip.load(Ordering::Acquire);
+        vcpu.set_regs(&[(Reg::Rip, eip as u64 & 0xffff)])?;
+        vcpu.set_sregs(
+            &[],
+            &[(
+                SegReg::Cs,
+                SegRegVal {
+                    selector: 0xf000,
+                    base: eip as u64 & 0xffff_0000,
+                    limit: 0xffff,
+                    access: SegAccess(0x9b),
+                },
+            )],
+            &[],
+        )?;
+        Ok(())
+    }
+
     pub fn init_boot_vcpu(&self, vcpu: &mut V::Vcpu, init_state: &InitState) -> Result<()> {
         vcpu.set_sregs(&init_state.sregs, &init_state.seg_regs, &init_state.dt_regs)?;
         vcpu.set_regs(&init_state.regs)?;
@@ -128,4 +170,40 @@ where
         }
         Ok(())
     }
+}
+
+const GUID_TABLE_FOOTER_R_OFFSET: usize = 48;
+
+const GUID_SIZE: usize = 16;
+
+const GUID_TABLE_FOOTER_GUID: [u8; GUID_SIZE] = [
+    0xDE, 0x82, 0xB5, 0x96, 0xB2, 0x1F, 0xF7, 0x45, 0xBA, 0xEA, 0xA3, 0x66, 0xC5, 0x5A, 0x08, 0x2D,
+];
+
+const SEV_ES_RESET_BLOCK_GUID: [u8; GUID_SIZE] = [
+    0xde, 0x71, 0xf7, 0x00, 0x7e, 0x1a, 0xcb, 0x4f, 0x89, 0x0e, 0x68, 0xc7, 0x7e, 0x2f, 0xb4, 0x4e,
+];
+
+pub fn get_ap_eip_from_fw(blob: &[u8]) -> Option<u32> {
+    let offset_table_footer = blob.len().checked_sub(GUID_TABLE_FOOTER_R_OFFSET)?;
+    if !blob[offset_table_footer..].starts_with(&GUID_TABLE_FOOTER_GUID) {
+        return None;
+    }
+    let offset_table_len = offset_table_footer.checked_sub(size_of::<u16>())?;
+    let table_len = u16::read_from_prefix(&blob[offset_table_len..])? as usize;
+    let offset_table_end = offset_table_len.checked_sub(
+        table_len
+            .checked_sub(size_of::<u16>())?
+            .checked_sub(GUID_SIZE)?,
+    )?;
+    let mut current = offset_table_len;
+    while current > offset_table_end {
+        let offset_len = current.checked_sub(GUID_SIZE + size_of::<u16>())?;
+        if blob[(offset_len + 2)..].starts_with(&SEV_ES_RESET_BLOCK_GUID) {
+            return u32::read_from_prefix(&blob[offset_len.checked_sub(size_of::<u32>())?..]);
+        }
+        let table_len = u16::read_from_prefix(&blob[offset_len..])? as usize;
+        current = current.checked_sub(table_len)?;
+    }
+    None
 }
