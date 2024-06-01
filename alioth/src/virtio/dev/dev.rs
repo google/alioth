google/alioth

commit 5ad2ea658cf234a85d65bda43c7fc761c2af0bd4

feat(virtio): enable ACCESS_PLATFORM bits
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/virtio.rs
@@ -51,8 +51,9 @@ bitflags! {
         const INDIRECT_DESC = 1 << 28;
         const EVENT_IDX = 1 << 29;
         const VERSION_1 = 1 << 32;
+        const ACCESS_PLATFORM = 1 << 33;
         const RING_PACKED = 1 << 34;
-        const SUPPORTED = Self::VERSION_1.bits();
+        const SUPPORTED = Self::VERSION_1.bits() | Self::ACCESS_PLATFORM.bits();
     }
 }
 

commit bbf891de853d212a67996b44070d0115b0c0d2b6

fix(virtio)!: enable ACCESS_PLATFORM for CoCo only
The bit VIRTIO_F_ACCESS_PLATFORM is a little bit confusing:

- when a Linux guest driver sees this bit, it uses DMA API for virtq
  transactions.
  - For a confidential VM, the DMA layer will use swiotlb to copy
    data from/to the shared memory.
  - In the non-CoCo case, I observed 5% performance regression of the
    virtio-net throughput.
- when an in-kernel vhost device sees this bit, addresses in the
  virtq are viewed as io virtual addresses and it expects the
  userspace VMM to setup the IOTLB.

We do not have an emulated IOMMU, so we should not set
ACCESS_PLATFORM on backend devices. On the other hand, in the CoCo
case, we must advertise this bit to the guest.

So for now, the solution is, we let the middle layer turn on
ACCESS_PLATFORM if necessary but never activate the device backend
with this bit. We will need to find a better solution in the future.

Fixes: 5ad2ea658cf2 ("feat(virtio): enable ACCESS_PLATFORM bits")

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/dev/dev.rs
@@ -168,14 +168,26 @@ where
         Ok(())
     }
 
-    pub fn new<R>(name: Arc<String>, dev: D, memory: Arc<RamBus>, registry: &R) -> Result<Self>
+    pub fn new<R>(
+        name: Arc<String>,
+        dev: D,
+        memory: Arc<RamBus>,
+        registry: &R,
+        restricted_memory: bool,
+    ) -> Result<Self>
     where
         R: IoeventFdRegistry<IoeventFd = E>,
     {
         let poll = Poll::new()?;
         let device_config = dev.config();
+        let mut dev_feat = dev.feature();
+        if restricted_memory {
+            dev_feat |= VirtioFeature::ACCESS_PLATFORM.bits()
+        } else {
+            dev_feat &= !VirtioFeature::ACCESS_PLATFORM.bits()
+        }
         let reg = Arc::new(Register {
-            device_feature: dev.feature(),
+            device_feature: dev_feat,
             ..Default::default()
         });
         let num_queues = dev.num_queues();
@@ -339,7 +351,7 @@ where
         let memory = &self.memory;
         self.dev.activate(
             self.poll.registry(),
-            feature,
+            feature & !VirtioFeature::ACCESS_PLATFORM.bits(),
             memory,
             irq_sender.as_ref(),
             &self.queue_regs,

alioth/src/virtio/virtio.rs
@@ -86,9 +86,7 @@ bitflags! {
     }
 }
 
-const FEATURE_BUILT_IN: u64 = VirtioFeature::EVENT_IDX.bits()
-    | VirtioFeature::VERSION_1.bits()
-    | VirtioFeature::ACCESS_PLATFORM.bits();
+const FEATURE_BUILT_IN: u64 = VirtioFeature::EVENT_IDX.bits() | VirtioFeature::VERSION_1.bits();
 
 #[derive(Debug, Clone, Copy)]
 pub enum DeviceId {

alioth/src/vm.rs
@@ -169,8 +169,13 @@ where
         let name = Arc::new(name);
         let dev = param.build(name.clone())?;
         let registry = self.board.vm.create_ioeventfd_registry()?;
-        let virtio_dev =
-            VirtioDevice::new(name.clone(), dev, self.board.memory.ram_bus(), &registry)?;
+        let virtio_dev = VirtioDevice::new(
+            name.clone(),
+            dev,
+            self.board.memory.ram_bus(),
+            &registry,
+            self.board.config.coco.is_some(),
+        )?;
         let msi_sender = self.board.vm.create_msi_sender()?;
         let dev = VirtioPciDevice::new(virtio_dev, msi_sender, registry)?;
         let dev = Arc::new(dev);