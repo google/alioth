google/alioth

commit a536818653815f3904301bfa1a2e0852c62955a2

feat(mem)!: create anonymous mem with memfd_create
The fd from memfd_create() enables Alioth to share the guest memory
with another process by sending the fd to the target process, which
is necessary for supporting vhost-user backends.

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/board/x86_64.rs
@@ -132,7 +132,7 @@ where
         let memory = &self.memory;
 
         let low_mem_size = std::cmp::min(config.mem_size, RAM_32_SIZE);
-        let pages_low = ArcMemPages::new_anon(low_mem_size)?;
+        let pages_low = ArcMemPages::from_anonymous(low_mem_size, None, Some(c"ram-low"))?;
         if self.config.coco.is_some() {
             self.memory.ram_bus().register_encrypted_pages(&pages_low)?;
         }
@@ -161,7 +161,11 @@ where
         };
         memory.add_region(AddrOpt::Fixed(0), Arc::new(region_low))?;
         if config.mem_size > RAM_32_SIZE {
-            let mem_hi = ArcMemPages::new_anon(config.mem_size - RAM_32_SIZE)?;
+            let mem_hi = ArcMemPages::from_anonymous(
+                config.mem_size - RAM_32_SIZE,
+                None,
+                Some(c"ram-high"),
+            )?;
             if self.config.coco.is_some() {
                 self.memory.ram_bus().register_encrypted_pages(&mem_hi)?;
             }

alioth/src/loader/firmware/x86_64.rs
@@ -29,7 +29,7 @@ pub fn load<P: AsRef<Path>>(memory: &Memory, path: P) -> Result<(InitState, ArcM
     let size = file.metadata()?.len() as usize;
     assert_eq!(size & 0xfff, 0);
 
-    let mut rom = ArcMemPages::new_anon(size)?;
+    let mut rom = ArcMemPages::from_anonymous(size, None, Some(c"rom"))?;
     file.read_exact(rom.as_slice_mut())?;
 
     let gpa = MEM_64_START - size;

alioth/src/mem/mapped.rs
@@ -13,18 +13,19 @@
 // limitations under the License.
 
 use std::cell::UnsafeCell;
+use std::ffi::CStr;
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::{IoSlice, IoSliceMut, Read, Write};
 use std::mem::{align_of, size_of};
 use std::ops::Deref;
-use std::os::fd::AsRawFd;
+use std::os::fd::{AsFd, AsRawFd, BorrowedFd, FromRawFd};
 use std::ptr::{null_mut, NonNull};
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 
 use libc::{
-    c_void, mmap, msync, munmap, MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE, MS_ASYNC, PROT_EXEC,
+    c_void, mmap, msync, munmap, MAP_FAILED, MAP_SHARED, MFD_CLOEXEC, MS_ASYNC, PROT_EXEC,
     PROT_READ, PROT_WRITE,
 };
 use parking_lot::{RwLock, RwLockReadGuard};
@@ -40,6 +41,7 @@ use super::{Error, Result};
 struct MemPages {
     addr: NonNull<c_void>,
     len: usize,
+    fd: File,
 }
 
 unsafe impl Send for MemPages {}
@@ -81,42 +83,43 @@ impl ArcMemPages {
         self.size
     }
 
+    pub fn fd(&self) -> BorrowedFd {
+        self._inner.fd.as_fd()
+    }
+
     pub fn sync(&self) -> Result<()> {
         ffi!(unsafe { msync(self.addr as *mut _, self.size, MS_ASYNC) })?;
         Ok(())
     }
 
-    fn new_raw(addr: *mut c_void, len: usize) -> Self {
+    fn from_raw(addr: *mut c_void, len: usize, fd: File) -> Self {
         let addr = NonNull::new(addr).expect("address from mmap() should not be null");
         ArcMemPages {
             addr: addr.as_ptr() as usize,
             size: len,
-            _inner: Arc::new(MemPages { addr, len }),
+            _inner: Arc::new(MemPages { addr, len, fd }),
         }
     }
 
-    pub fn new_file(file: File) -> Result<Self, Error> {
-        let mut prot = PROT_READ | PROT_EXEC;
-        let meta = file.metadata().map_err(Error::Mmap)?;
-        let is_readonly = meta.permissions().readonly();
-        if !is_readonly {
-            prot |= PROT_WRITE;
-        }
-        let size = meta.len() as usize;
-        let addr = unsafe { mmap(null_mut(), size, prot, MAP_PRIVATE, file.as_raw_fd(), 0) };
-        match addr {
-            MAP_FAILED => Err(Error::Mmap(std::io::Error::last_os_error())),
-            addr => Ok(Self::new_raw(addr, size)),
-        }
+    pub fn from_file(file: File, offset: i64, len: usize, prot: i32) -> Result<Self> {
+        let addr = ffi!(
+            unsafe { mmap(null_mut(), len, prot, MAP_SHARED, file.as_raw_fd(), offset) },
+            MAP_FAILED
+        )?;
+        Ok(Self::from_raw(addr, len, file))
     }
 
-    pub fn new_anon(size: usize) -> Result<Self, Error> {
-        let prot = PROT_WRITE | PROT_READ | PROT_EXEC;
-        let addr = unsafe { mmap(null_mut(), size, prot, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0) };
-        match addr {
-            MAP_FAILED => Err(Error::Mmap(std::io::Error::last_os_error())),
-            addr => Ok(Self::new_raw(addr, size)),
-        }
+    pub fn from_anonymous(size: usize, prot: Option<i32>, name: Option<&CStr>) -> Result<Self> {
+        let name = name.unwrap_or(c"anon");
+        let fd = ffi!(unsafe { libc::memfd_create(name.as_ptr(), MFD_CLOEXEC) })?;
+        let prot = prot.unwrap_or(PROT_WRITE | PROT_READ | PROT_EXEC);
+        let addr = ffi!(
+            unsafe { mmap(null_mut(), size, prot, MAP_SHARED, fd, 0) },
+            MAP_FAILED
+        )?;
+        let file = unsafe { File::from_raw_fd(fd) };
+        file.set_len(size as _)?;
+        Ok(Self::from_raw(addr, size, file))
     }
 
     /// Given offset and len, return the host virtual address and len;
@@ -553,9 +556,8 @@ mod test {
     use assert_matches::assert_matches;
     use std::io::{Read, Write};
     use std::mem::size_of;
-    use std::ptr::null_mut;
 
-    use libc::{mmap, munmap, MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE, PROT_READ, PROT_WRITE};
+    use libc::{PROT_READ, PROT_WRITE};
     use zerocopy::{AsBytes, FromBytes, FromZeroes};
 
     use crate::hv::test::FakeVmMemory;
@@ -574,17 +576,16 @@ mod test {
     fn test_ram_bus_read() {
         let bus = RamBus::new(FakeVmMemory);
         let prot = PROT_READ | PROT_WRITE;
-        let size = 3 * PAGE_SIZE;
-        let addr = unsafe { mmap(null_mut(), size, prot, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0) };
-        assert_ne!(addr, MAP_FAILED);
-        let munmap_ret = unsafe { munmap(addr.add(PAGE_SIZE), PAGE_SIZE) };
-        assert_ne!(munmap_ret, -1);
-        let mem1 = ArcMemPages::new_raw(addr, PAGE_SIZE);
-        let mem2_addr = unsafe { addr.add(2 * PAGE_SIZE) };
-        let mem2 = ArcMemPages::new_raw(mem2_addr, PAGE_SIZE);
+        let mem1 = ArcMemPages::from_anonymous(PAGE_SIZE, Some(prot), None).unwrap();
+        let mem2 = ArcMemPages::from_anonymous(PAGE_SIZE, Some(prot), None).unwrap();
 
-        bus.add(0x0, mem1).unwrap();
-        bus.add(PAGE_SIZE, mem2).unwrap();
+        if mem1.addr > mem2.addr {
+            bus.add(0x0, mem1).unwrap();
+            bus.add(PAGE_SIZE, mem2).unwrap();
+        } else {
+            bus.add(0x0, mem2).unwrap();
+            bus.add(PAGE_SIZE, mem1).unwrap();
+        }
 
         let data = MyStruct {
             data: [1, 2, 3, 4, 5, 6, 7, 8],

commit 955212af7f82557e29931334e2d23df41f61c758

fix(mem): add private anonymous memory back
Private anonymous memory is useful when a device does not want other
devices to access its memory.

Fixes: a53681865381 ("feat(mem)!: create anonymous mem with memfd_create")

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/board/x86_64.rs
@@ -132,7 +132,7 @@ where
         let memory = &self.memory;
 
         let low_mem_size = std::cmp::min(config.mem_size, RAM_32_SIZE);
-        let pages_low = ArcMemPages::from_anonymous(low_mem_size, None, Some(c"ram-low"))?;
+        let pages_low = ArcMemPages::from_memfd(low_mem_size, None, Some(c"ram-low"))?;
         if self.config.coco.is_some() {
             self.memory.ram_bus().register_encrypted_pages(&pages_low)?;
         }
@@ -161,11 +161,8 @@ where
         };
         memory.add_region(AddrOpt::Fixed(0), Arc::new(region_low))?;
         if config.mem_size > RAM_32_SIZE {
-            let mem_hi = ArcMemPages::from_anonymous(
-                config.mem_size - RAM_32_SIZE,
-                None,
-                Some(c"ram-high"),
-            )?;
+            let mem_hi =
+                ArcMemPages::from_memfd(config.mem_size - RAM_32_SIZE, None, Some(c"ram-high"))?;
             if self.config.coco.is_some() {
                 self.memory.ram_bus().register_encrypted_pages(&mem_hi)?;
             }

alioth/src/loader/firmware/x86_64.rs
@@ -29,7 +29,7 @@ pub fn load<P: AsRef<Path>>(memory: &Memory, path: P) -> Result<(InitState, ArcM
     let size = file.metadata()?.len() as usize;
     assert_eq!(size & 0xfff, 0);
 
-    let mut rom = ArcMemPages::from_anonymous(size, None, Some(c"rom"))?;
+    let mut rom = ArcMemPages::from_memfd(size, None, Some(c"rom"))?;
     file.read_exact(rom.as_slice_mut())?;
 
     let gpa = MEM_64_START - size;

alioth/src/mem/mapped.rs
@@ -25,8 +25,8 @@ use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 
 use libc::{
-    c_void, mmap, msync, munmap, MAP_FAILED, MAP_SHARED, MFD_CLOEXEC, MS_ASYNC, PROT_EXEC,
-    PROT_READ, PROT_WRITE,
+    c_void, mmap, msync, munmap, MAP_ANONYMOUS, MAP_FAILED, MAP_PRIVATE, MAP_SHARED, MFD_CLOEXEC,
+    MS_ASYNC, PROT_EXEC, PROT_READ, PROT_WRITE,
 };
 use parking_lot::{RwLock, RwLockReadGuard};
 use zerocopy::{AsBytes, FromBytes};
@@ -41,7 +41,7 @@ use super::{Error, Result};
 struct MemPages {
     addr: NonNull<c_void>,
     len: usize,
-    fd: File,
+    fd: Option<File>,
 }
 
 unsafe impl Send for MemPages {}
@@ -83,8 +83,8 @@ impl ArcMemPages {
         self.size
     }
 
-    pub fn fd(&self) -> BorrowedFd {
-        self._inner.fd.as_fd()
+    pub fn fd(&self) -> Option<BorrowedFd> {
+        self._inner.fd.as_ref().map(|f| f.as_fd())
     }
 
     pub fn sync(&self) -> Result<()> {
@@ -92,7 +92,7 @@ impl ArcMemPages {
         Ok(())
     }
 
-    fn from_raw(addr: *mut c_void, len: usize, fd: File) -> Self {
+    fn from_raw(addr: *mut c_void, len: usize, fd: Option<File>) -> Self {
         let addr = NonNull::new(addr).expect("address from mmap() should not be null");
         ArcMemPages {
             addr: addr.as_ptr() as usize,
@@ -106,10 +106,10 @@ impl ArcMemPages {
             unsafe { mmap(null_mut(), len, prot, MAP_SHARED, file.as_raw_fd(), offset) },
             MAP_FAILED
         )?;
-        Ok(Self::from_raw(addr, len, file))
+        Ok(Self::from_raw(addr, len, Some(file)))
     }
 
-    pub fn from_anonymous(size: usize, prot: Option<i32>, name: Option<&CStr>) -> Result<Self> {
+    pub fn from_memfd(size: usize, prot: Option<i32>, name: Option<&CStr>) -> Result<Self> {
         let name = name.unwrap_or(c"anon");
         let fd = ffi!(unsafe { libc::memfd_create(name.as_ptr(), MFD_CLOEXEC) })?;
         let prot = prot.unwrap_or(PROT_WRITE | PROT_READ | PROT_EXEC);
@@ -119,7 +119,16 @@ impl ArcMemPages {
         )?;
         let file = unsafe { File::from_raw_fd(fd) };
         file.set_len(size as _)?;
-        Ok(Self::from_raw(addr, size, file))
+        Ok(Self::from_raw(addr, size, Some(file)))
+    }
+
+    pub fn from_anonymous(size: usize, prot: Option<i32>) -> Result<Self> {
+        let prot = prot.unwrap_or(PROT_WRITE | PROT_READ | PROT_EXEC);
+        let addr = ffi!(
+            unsafe { mmap(null_mut(), size, prot, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0) },
+            MAP_FAILED
+        )?;
+        Ok(Self::from_raw(addr, size, None))
     }
 
     /// Given offset and len, return the host virtual address and len;
@@ -581,8 +590,8 @@ mod test {
     fn test_ram_bus_read() {
         let bus = RamBus::new(FakeVmMemory);
         let prot = PROT_READ | PROT_WRITE;
-        let mem1 = ArcMemPages::from_anonymous(PAGE_SIZE, Some(prot), None).unwrap();
-        let mem2 = ArcMemPages::from_anonymous(PAGE_SIZE, Some(prot), None).unwrap();
+        let mem1 = ArcMemPages::from_memfd(PAGE_SIZE, Some(prot), None).unwrap();
+        let mem2 = ArcMemPages::from_memfd(PAGE_SIZE, Some(prot), None).unwrap();
 
         if mem1.addr > mem2.addr {
             bus.add(0x0, mem1).unwrap();

alioth/src/virtio/dev/fs.rs
@@ -156,6 +156,9 @@ impl Virtio for VuFs {
             .set_features(&(feature | VirtioFeature::VHOST_PROTOCOL.bits()))?;
         let mem = memory.lock_layout();
         for (gpa, slot) in mem.iter() {
+            let Some(fd) = slot.pages.fd() else {
+                continue;
+            };
             let region = MemorySingleRegion {
                 _padding: 0,
                 region: MemoryRegion {
@@ -165,9 +168,7 @@ impl Virtio for VuFs {
                     mmap_offset: 0,
                 },
             };
-            self.vu_dev
-                .add_mem_region(&region, slot.pages.fd().as_raw_fd())
-                .unwrap();
+            self.vu_dev.add_mem_region(&region, fd.as_raw_fd()).unwrap();
             log::info!("region: {region:x?}");
             self.regions.push(region.region);
         }