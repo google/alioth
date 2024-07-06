google/alioth

commit bc5ee80a77ce1b1dc479258cdb2eaecf9a8ccc20

fix(pci): do not add resources to memory at boot
As per PCIe Spec v6.1 Sec 7.5.1.1.3 Table 7-4, the default values of
the memory and I/O space bits of the Command register are 0.

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/board/board.rs
@@ -25,7 +25,9 @@ use std::thread::JoinHandle;
 use parking_lot::{Condvar, Mutex, RwLock, RwLockReadGuard};
 use thiserror::Error;
 
-use crate::arch::layout::PCIE_CONFIG_START;
+use crate::arch::layout::{
+    MEM_64_START, PCIE_CONFIG_START, PCIE_MMIO_32_END, PCIE_MMIO_32_START, RAM_32_SIZE,
+};
 use crate::device::fw_cfg::FwCfg;
 use crate::hv::{self, Coco, Vcpu, Vm, VmEntry, VmExit};
 #[cfg(target_arch = "x86_64")]
@@ -33,11 +35,10 @@ use crate::loader::xen;
 use crate::loader::{self, firmware, linux, ExecType, InitState, Payload};
 use crate::mem::emulated::Mmio;
 use crate::mem::{self, AddrOpt, MemRegion, MemRegionType, Memory};
+use crate::pci;
 use crate::pci::bus::PciBus;
 #[cfg(target_arch = "x86_64")]
 use crate::pci::bus::CONFIG_ADDRESS;
-use crate::pci::config::Command;
-use crate::pci::{self, PciBar};
 
 #[cfg(target_arch = "aarch64")]
 pub(crate) use aarch64::ArchBoard;
@@ -78,10 +79,18 @@ pub const STATE_RUNNING: u8 = 1;
 pub const STATE_SHUTDOWN: u8 = 2;
 pub const STATE_REBOOT_PENDING: u8 = 3;
 
+pub const PCIE_MMIO_64_SIZE: u64 = 1 << 40;
+
 pub struct BoardConfig {
     pub mem_size: u64,
     pub num_cpu: u32,
     pub coco: Option<Coco>,
+}
+
+impl BoardConfig {
+    pub fn pcie_mmio_64_start(&self) -> u64 {
+        (self.mem_size.saturating_sub(RAM_32_SIZE) + MEM_64_START).next_power_of_two()
+    }
 }
 
 type VcpuGuard<'a> = RwLockReadGuard<'a, Vec<(JoinHandle<Result<()>>, Sender<()>)>>;
@@ -151,29 +160,14 @@ where
                 MemRegionType::Reserved,
             )),
         )?;
-        let devices = self.pci_bus.segment.devices.read();
-        for (_, dev) in devices.iter() {
-            let config = dev.dev.config();
-            let header = config.get_header();
-            for (index, bar) in header.bars.iter().enumerate() {
-                match bar {
-                    PciBar::Empty => {}
-                    PciBar::Mem32(region) => {
-                        let addr = self.memory.add_region(AddrOpt::Below4G, region.clone())?;
-                        log::info!("{}: BAR {index} located at {addr:#x}", dev.name);
-                    }
-                    PciBar::Mem64(region) => {
-                        let addr = self.memory.add_region(AddrOpt::Above4G, region.clone())?;
-                        log::info!("{}: BAR {index} located at {addr:#x}", dev.name);
-                    }
-                    PciBar::Io(region) => {
-                        let addr = self.memory.add_io_region(None, region.clone())?;
-                        log::info!("{}: IO BAR {index} located at {addr:#x}", dev.name);
-                    }
-                }
-            }
-            header.set_command(Command::MEM | Command::IO);
-        }
+        self.pci_bus.assign_resources(&[
+            (0x1000, 0x10000),
+            (PCIE_MMIO_32_START, PCIE_MMIO_32_END),
+            (
+                self.config.pcie_mmio_64_start(),
+                self.config.pcie_mmio_64_start() + PCIE_MMIO_64_SIZE,
+            ),
+        ]);
         Ok(())
     }
 

alioth/src/pci/config.rs
@@ -223,6 +223,12 @@ impl HeaderData {
         }
     }
 
+    pub fn get_bar(&self, index: usize) -> Option<(u32, u32)> {
+        match &self.header {
+            ConfigHeader::Device(header) => Some((header.bars[index], self.bar_masks[index])),
+        }
+    }
+
     pub fn set_command(&mut self, command: Command) {
         match &mut self.header {
             ConfigHeader::Device(header) => header.common.command = command,

alioth/src/arch/aarch64/layout.rs
@@ -35,5 +35,8 @@ pub const KERNEL_IMAGE_START: u64 = 0x4020_0000; // 1 GiB + 2 MiB
 pub const RAM_32_END: u64 = 0xc000_0000; // 3 GiB
 pub const RAM_32_SIZE: u64 = RAM_32_END - RAM_32_START; // 2 GiB
 
+pub const PCIE_MMIO_32_START: u64 = 0xc000_0000; // 3 GiB
+pub const PCIE_MMIO_32_END: u64 = 0x1_0000_0000; // 4 GiB, size = 1 GiB
+
 pub const MEM_64_START: u64 = 0x1_0000_0000; // 4GiB
 pub const PAGE_SIZE: u64 = 0x1000; // 4KiB

alioth/src/arch/x86_64/layout.rs
@@ -39,8 +39,11 @@ pub const KERNEL_IMAGE_START: u64 = 0x100_0000; // 16 MiB
 pub const RAM_32_END: u64 = 0x8000_0000; // 2 GiB
 pub const RAM_32_SIZE: u64 = RAM_32_END; // 2 GiB
 
-pub const MMIO_32_START: u64 = 0x8000_0000; // 2 GiB
-pub const MMIO_32_END: u64 = 0xe000_0000; // 3.5 GiB
+pub const PCIE_MMIO_32_START: u64 = 0x8000_0000; // 2 GiB
+pub const PCIE_MMIO_32_END: u64 = 0xc000_0000; // 3 GiB, size = 1 GiB
+
+pub const MMIO_32_START: u64 = 0xc000_0000; // 3 GiB
+pub const MMIO_32_END: u64 = 0xe000_0000; // 3.5 GiB, size = 512 MiB
 
 pub const PCIE_CONFIG_START: u64 = 0xe000_0000; // 3.5 GiB
 pub const PCIE_CONFIG_END: u64 = 0xf000_0000; // 3.75 GiB, size = 256 MiB

alioth/src/pci/bus.rs
@@ -13,17 +13,19 @@
 // limitations under the License.
 
 use std::collections::HashMap;
+use std::iter::zip;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 
 use bitfield::bitfield;
 use parking_lot::{Mutex, RwLock};
 
-use crate::mem;
 use crate::mem::emulated::Mmio;
+use crate::pci::config::{BAR_IO, BAR_MEM64};
 use crate::pci::host_bridge::HostBridge;
 use crate::pci::segment::PciSegment;
 use crate::pci::{Bdf, PciDevice, Result};
+use crate::{align_up, mem};
 
 pub const CONFIG_ADDRESS: u16 = 0xcf8;
 pub const CONFIG_DATA: u16 = 0xcfc;
@@ -123,6 +125,71 @@ impl PciBus {
 
     pub fn add(&self, bdf: Bdf, dev: PciDevice) -> Option<PciDevice> {
         self.segment.add(bdf, dev)
+    }
+
+    /// Assigns addresses to all devices' base address registers
+    ///
+    /// `resources` is an array of 3 `(start, end)` tuples, corresponds to
+    ///
+    /// - IO space,
+    /// - 32-bit memory space,
+    /// - 64-bit memory space,
+    ///
+    /// respectively.
+    pub fn assign_resources(&self, resources: &[(u64, u64); 3]) {
+        let mut bar_lists = [const { vec![] }; 3];
+        let devices = self.segment.devices.read();
+        for (bdf, dev) in devices.iter() {
+            let config = dev.dev.config();
+            let header = config.get_header().data.read();
+            let mut index = 0;
+            while index < 6 {
+                let bar_index = index;
+                index += 1;
+                let Some((val, mask)) = header.get_bar(bar_index) else {
+                    continue;
+                };
+                let mut mask = mask as u64;
+                if val & BAR_MEM64 == BAR_MEM64 {
+                    let (_, mask_hi) = header.get_bar(bar_index + 1).unwrap();
+                    mask |= (mask_hi as u64) << 32;
+                    index += 1;
+                }
+                if mask == 0 {
+                    continue;
+                }
+                let bar_list = if val & BAR_IO == BAR_IO {
+                    &mut bar_lists[0]
+                } else if val & BAR_MEM64 == BAR_MEM64 {
+                    &mut bar_lists[2]
+                } else {
+                    &mut bar_lists[1]
+                };
+                bar_list.push((*bdf, dev, bar_index, 1 << mask.trailing_zeros()));
+            }
+        }
+        for bar_list in bar_lists.iter_mut() {
+            bar_list.sort_by_key(|(bdf, _, index, size)| (u64::MAX - size, *bdf, *index));
+        }
+        for (bar_list, (start, end)) in zip(bar_lists, resources) {
+            let mut addr = *start;
+            for (bdf, dev, index, size) in bar_list {
+                let config = dev.dev.config();
+                let mut header = config.get_header().data.write();
+                let aligned_addr = align_up!(addr, size);
+                if aligned_addr + size > *end {
+                    log::error!(
+                        "{bdf}: cannot map BAR {index} into address range {start:#x}..{end:#x}"
+                    );
+                    continue;
+                }
+                header.set_bar(index, aligned_addr as u32);
+                if aligned_addr > u32::MAX as u64 {
+                    header.set_bar(index + 1, (aligned_addr >> 32) as u32);
+                }
+                addr = aligned_addr + size;
+            }
+        }
     }
 }
 

commit 69c17c8d421c48929cf7dfd89cd069542c42e42a

fix(board)!: reset PCI device config space
Without the fix, after a reboot, while PCI device resources have been
removed from the memory bus, the config space Command register may still
have the MEM and IO bit on.

Fixes: bc5ee80a77ce ("fix(pci): do not add resources to memory at boot")

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/board/board.rs
@@ -302,6 +302,7 @@ where
                 let devices = self.pci_bus.segment.devices.read();
                 for (bdf, dev) in devices.iter() {
                     dev.dev.reset().context(error::ResetPci { bdf: *bdf })?;
+                    dev.dev.config().reset();
                 }
                 self.memory.reset()?;
             }

alioth/src/mem/emulated.rs
@@ -109,7 +109,7 @@ pub struct MmioBus<R = MmioRange>
 where
     R: Debug + SlotBackend,
 {
-    inner: RwLock<Addressable<R>>,
+    pub(crate) inner: RwLock<Addressable<R>>,
 }
 
 impl<R> Default for MmioBus<R>

alioth/src/pci/cap.rs
@@ -126,6 +126,7 @@ impl Default for MsixTableEntry {
 
 pub trait PciCap: Mmio {
     fn set_next(&mut self, val: u8);
+    fn reset(&self);
 }
 
 impl SlotBackend for Box<dyn PciCap> {
@@ -168,6 +169,13 @@ impl PciCapList {
 
     pub fn is_empty(&self) -> bool {
         self.inner.is_empty()
+    }
+
+    pub fn reset(&self) {
+        let inner = self.inner.inner.read();
+        for (_, cap) in inner.iter() {
+            cap.reset();
+        }
     }
 }
 
@@ -234,6 +242,12 @@ impl Mmio for MsixCapMmio {
 impl PciCap for MsixCapMmio {
     fn set_next(&mut self, val: u8) {
         self.cap.write().header.next = val;
+    }
+
+    fn reset(&self) {
+        let mut cap = self.cap.write();
+        cap.control.set_enabled(false);
+        cap.control.set_masked(false);
     }
 }
 

alioth/src/pci/config.rs
@@ -358,6 +358,11 @@ impl EmulatedHeader {
         let mut header = self.data.write();
         header.set_command(command)
     }
+
+    pub fn reset(&self) {
+        let mut header = self.data.write();
+        header.set_command(Command::empty());
+    }
 }
 
 impl Mmio for EmulatedHeader {
@@ -393,6 +398,7 @@ impl Mmio for EmulatedHeader {
 
 pub trait PciConfig: Mmio {
     fn get_header(&self) -> &EmulatedHeader;
+    fn reset(&self);
 }
 
 #[derive(Debug)]
@@ -449,5 +455,10 @@ impl EmulatedConfig {
 impl PciConfig for EmulatedConfig {
     fn get_header(&self) -> &EmulatedHeader {
         &self.header
+    }
+
+    fn reset(&self) {
+        self.header.reset();
+        self.caps.reset();
     }
 }

alioth/src/virtio/pci.rs
@@ -593,6 +593,8 @@ impl PciCap for VirtioPciCap {
     fn set_next(&mut self, val: u8) {
         self.header.next = val
     }
+
+    fn reset(&self) {}
 }
 
 #[repr(C, align(4))]
@@ -608,6 +610,7 @@ impl PciCap for VirtioPciCap64 {
     fn set_next(&mut self, val: u8) {
         PciCap::set_next(&mut self.cap, val)
     }
+    fn reset(&self) {}
 }
 
 #[repr(C, align(4))]
@@ -622,6 +625,7 @@ impl PciCap for VirtioPciNotifyCap {
     fn set_next(&mut self, val: u8) {
         self.cap.header.next = val;
     }
+    fn reset(&self) {}
 }
 
 #[derive(Debug)]