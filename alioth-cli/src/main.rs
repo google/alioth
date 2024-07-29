google/alioth

commit 6381397d56570151324eb46134175cd7fd8696df

doc(cli): add help message to the flag --object
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth-cli/src/main.rs
@@ -108,6 +108,21 @@ enum VsockParam {
     Vhost(VhostVsockParam),
 }
 
+const DOC_OBJECTS: &str = r#"Supply additional data to other command line flags.
+* <id>,<value>
+
+Any value that comes after an equal sign(=) and contains a comma(,)
+or equal sign can be supplied using this flag. `<id>` must start
+with `id_` and `<id>` cannot contain any comma or equal sign.
+
+Example: assuming we are going a add a virtio-blk device backed by
+`/path/to/disk,2024.img` and a virtio-fs device backed by a
+vhost-user process listening on socket `/path/to/socket=1`, these
+2 devices can be expressed in the command line as follows:
+    --blk path=id_blk --fs vu,socket=id_fs,tag=shared-dir \
+    -o id_blk,/path/to/disk,2024.img \
+    -o id_fs,socket=/path/to/socket=1"#;
+
 #[derive(Args, Debug, Clone)]
 struct RunArgs {
     #[arg(long, help(
@@ -188,7 +203,7 @@ struct RunArgs {
     ))]
     vfio: Vec<String>,
 
-    #[arg(short, long("object"))]
+    #[arg(short, long("object"), help = DOC_OBJECTS, value_name = "OBJECT")]
     objects: Vec<String>,
 }
 

commit 3f473f91c50405c1ca6a5d66a8152bc30f0366ac

doc(cli): fix the example in `--object` help
Fixes: 6381397d5657 ("doc(cli): add help message to the flag --object")

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth-cli/src/main.rs
@@ -124,7 +124,7 @@ vhost-user process listening on socket `/path/to/socket=1`, these
 2 devices can be expressed in the command line as follows:
     --blk path=id_blk --fs vu,socket=id_fs,tag=shared-dir \
     -o id_blk,/path/to/disk,2024.img \
-    -o id_fs,socket=/path/to/socket=1"#;
+    -o id_fs,/path/to/socket=1"#;
 
 #[derive(Args, Debug, Clone)]
 #[command(arg_required_else_help = true)]