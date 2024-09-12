google/alioth

commit c988169b27585f834640172f477055e83503a6b6

fix(kvm): clear the high part of mmio write values
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/hv/kvm/vmexit.rs
@@ -23,10 +23,12 @@ impl KvmVcpu {
     #[cfg(target_endian = "little")]
     pub(super) fn handle_mmio(&mut self) -> Result<VmExit, Error> {
         let kvm_mmio = unsafe { &self.kvm_run.exit.mmio };
+        let data = u64::from_ne_bytes(kvm_mmio.data)
+            & 1u64.wrapping_shl(kvm_mmio.len << 3).wrapping_sub(1);
         let exit = VmExit::Mmio {
             addr: kvm_mmio.phys_addr,
             write: if kvm_mmio.is_write > 0 {
-                Some(u64::from_ne_bytes(kvm_mmio.data))
+                Some(data)
             } else {
                 None
             },

commit 2e6b7951a6ea1d65e422f67bfe98cc326941164e

fix(kvm): calculate correct mask for 64bit MMIO
1u64.wrapping_shl(64) returns 1 instead of 0. Without the fix, for 64bit
MMIO, the mask is 0 instead of u64::MAX.

Fixes: c988169b2758 ("fix(kvm): clear the high part of mmio write values")
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/hv/kvm/vmexit.rs
@@ -23,8 +23,7 @@ impl KvmVcpu {
     #[cfg(target_endian = "little")]
     pub(super) fn handle_mmio(&mut self) -> Result<VmExit, Error> {
         let kvm_mmio = unsafe { &self.kvm_run.exit.mmio };
-        let data = u64::from_ne_bytes(kvm_mmio.data)
-            & 1u64.wrapping_shl(kvm_mmio.len << 3).wrapping_sub(1);
+        let data = u64::from_ne_bytes(kvm_mmio.data) & u64::MAX >> (64 - (kvm_mmio.len << 3));
         let exit = VmExit::Mmio {
             addr: kvm_mmio.phys_addr,
             write: if kvm_mmio.is_write > 0 {