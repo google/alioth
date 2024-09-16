9643885cb5767448e5d721205edb15197cc1ecc3
Changyuan Lyu <changyuanl@google.com>
2024-09-15 21:48:53 -0700

    refactor(virtio): simplify method parameters of trait `VirtioMio`
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- alioth/src/virtio/dev/net/net.rs
+++ alioth/src/virtio/dev/net/net.rs
@@ -43,7 +43,7 @@ use crate::virtio::dev::{DevParam, DeviceId, Result, Virtio, WakeEvent};
 use crate::virtio::queue::handlers::{handle_desc, queue_to_writer, reader_to_queue};
 use crate::virtio::queue::{Descriptor, Queue, VirtQueue};
 use crate::virtio::worker::io_uring::{BufferAction, IoUring, VirtioIoUring};
-use crate::virtio::worker::mio::{Mio, VirtioMio};
+use crate::virtio::worker::mio::{ActiveMio, Mio, VirtioMio};
 use crate::virtio::worker::{Waker, WorkerApi};
 use crate::virtio::{error, IrqSender, FEATURE_BUILT_IN};
 use crate::{c_enum, impl_mmio_for_zerocopy};
@@ -340,18 +340,15 @@ impl VirtioMio for Net {
         let _ = registry.deregister(&mut SourceFd(&self.tap_sockets[0].as_raw_fd()));
     }
 
-    fn activate<'m, S: IrqSender, Q: VirtQueue<'m>>(
+    fn activate<'a, 'm, Q: VirtQueue<'m>, S: IrqSender>(
         &mut self,
-        registry: &Registry,
         feature: u64,
-        _memory: &'m Ram,
-        _irq_sender: &S,
-        _queues: &mut [Option<Q>],
+        active_mio: &mut ActiveMio<'a, 'm, Q, S>,
     ) -> Result<()> {
         self.driver_feature = NetFeature::from_bits_retain(feature);
         let socket = &mut self.tap_sockets[0];
         enable_tap_offload(socket, self.driver_feature)?;
-        registry.register(
+        active_mio.poll.registry().register(
             &mut SourceFd(&socket.as_raw_fd()),
             Token(0),
             Interest::READABLE | Interest::WRITABLE,
@@ -359,20 +356,16 @@ impl VirtioMio for Net {
         Ok(())
     }
 
-    fn handle_event<'m, Q>(
+    fn handle_event<'a, 'm, Q: VirtQueue<'m>, S: IrqSender>(
         &mut self,
         event: &Event,
-        queues: &mut [Option<Q>],
-        irq_sender: &impl IrqSender,
-        _registry: &Registry,
-    ) -> Result<()>
-    where
-        Q: VirtQueue<'m>,
-    {
+        active_mio: &mut ActiveMio<'a, 'm, Q, S>,
+    ) -> Result<()> {
         let token = event.token().0;
+        let irq_sender = active_mio.irq_sender;
         if event.is_readable() {
             let rx_queue_index = token << 1;
-            let Some(Some(queue)) = queues.get_mut(rx_queue_index) else {
+            let Some(Some(queue)) = active_mio.queues.get_mut(rx_queue_index) else {
                 log::error!("{}: cannot find rx queue {rx_queue_index}", self.name);
                 return Ok(());
             };
@@ -384,7 +377,7 @@ impl VirtioMio for Net {
         }
         if event.is_writable() {
             let tx_queue_index = (token << 1) + 1;
-            let Some(Some(queue)) = queues.get_mut(tx_queue_index) else {
+            let Some(Some(queue)) = active_mio.queues.get_mut(tx_queue_index) else {
                 log::error!("{}: cannot find tx queue {tx_queue_index}", self.name);
                 return Ok(());
             };
@@ -397,20 +390,17 @@ impl VirtioMio for Net {
         Ok(())
     }
 
-    fn handle_queue<'m, Q>(
+    fn handle_queue<'a, 'm, Q: VirtQueue<'m>, S: IrqSender>(
         &mut self,
         index: u16,
-        queues: &mut [Option<Q>],
-        irq_sender: &impl IrqSender,
-        registry: &Registry,
-    ) -> Result<()>
-    where
-        Q: VirtQueue<'m>,
-    {
-        let Some(Some(queue)) = queues.get_mut(index as usize) else {
+        active_mio: &mut ActiveMio<'a, 'm, Q, S>,
+    ) -> Result<()> {
+        let Some(Some(queue)) = active_mio.queues.get_mut(index as usize) else {
             log::error!("{}: invalid queue index {index}", self.name);
             return Ok(());
         };
+        let irq_sender = active_mio.irq_sender;
+        let registry = active_mio.poll.registry();
         if index == self.config.max_queue_pairs * 2 {
             let name = self.name.clone();
             return handle_desc(&name, index, queue, irq_sender, |desc| {
