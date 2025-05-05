5caa958dd3094e6a377d235dde59c442c4805dfe
Changyuan Lyu <changyuanl@google.com>
2025-05-04 17:27:58 -0700

    feat(fuse): define trait Fuse
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>

--- alioth/src/fuse/fuse.rs
+++ alioth/src/fuse/fuse.rs
@@ -13,3 +13,84 @@
 // limitations under the License.
 
 pub mod bindings;
+
+use std::io::{Error as IoError, IoSliceMut};
+
+use macros::trace_error;
+use snafu::Snafu;
+
+use crate::errors::DebugTrace;
+
+use self::bindings::{
+    FuseAttrOut, FuseEntryOut, FuseFlushIn, FuseForgetIn, FuseGetattrIn, FuseInHeader, FuseInitIn,
+    FuseInitOut, FuseIoctlIn, FuseIoctlOut, FuseOpenIn, FuseOpenOut, FusePollIn, FusePollOut,
+    FuseReadIn, FuseReleaseIn, FuseSyncfsIn,
+};
+
+#[trace_error]
+#[derive(Snafu, DebugTrace)]
+#[snafu(module, context(suffix(false)))]
+pub enum Error {
+    #[snafu(display("Error from OS"), context(false))]
+    System { error: std::io::Error },
+}
+
+impl Error {
+    pub fn error_code(&self) -> i32 {
+        match self {
+            Error::System { error, .. } => error.raw_os_error().unwrap_or(libc::EINVAL),
+        }
+    }
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+macro_rules! fuse_no_impl {
+    ($hdr:expr, $in_:expr) => {{
+        log::debug!("unimplemented: hdr: {:?}, in: {:?}", $hdr, $in_);
+        let err: IoError = IoError::from_raw_os_error(libc::ENOSYS);
+        Err(Error::from(err))
+    }};
+}
+
+macro_rules! fuse_method {
+    ($name:ident, $in_ty:ty,[]) => {
+        fn $name(&mut self, hdr: &FuseInHeader, in_: &$in_ty, _buf: &mut [u8]) -> Result<usize> {
+            fuse_no_impl!(hdr, in_)
+        }
+    };
+    ($name:ident, $in_ty:ty,[[]]) => {
+        fn $name(
+            &mut self,
+            hdr: &FuseInHeader,
+            in_: &$in_ty,
+            _iov: &mut [IoSliceMut],
+        ) -> Result<usize> {
+            fuse_no_impl!(hdr, in_)
+        }
+    };
+    ($name:ident, $in_ty:ty, $out_ty:ty) => {
+        fn $name(&mut self, hdr: &FuseInHeader, in_: &$in_ty) -> Result<$out_ty> {
+            fuse_no_impl!(hdr, in_)
+        }
+    };
+}
+
+pub trait Fuse {
+    fuse_method!(init, FuseInitIn, FuseInitOut);
+    fuse_method!(get_attr, FuseGetattrIn, FuseAttrOut);
+    fuse_method!(open, FuseOpenIn, FuseOpenOut);
+    fuse_method!(open_dir, FuseOpenIn, FuseOpenOut);
+    fuse_method!(read_dir, FuseReadIn, []);
+    fuse_method!(release_dir, FuseReleaseIn, ());
+    fuse_method!(lookup, [u8], FuseEntryOut);
+    fuse_method!(forget, FuseForgetIn, ());
+    fuse_method!(read, FuseReadIn, [[]]);
+    fuse_method!(poll, FusePollIn, FusePollOut);
+    fuse_method!(flush, FuseFlushIn, ());
+    fuse_method!(release, FuseReleaseIn, ());
+    fuse_method!(syncfs, FuseSyncfsIn, ());
+    fuse_method!(ioctl, FuseIoctlIn, FuseIoctlOut);
+    fuse_method!(get_xattr, [u8], []);
+    fuse_method!(set_xattr, [u8], ());
+}
