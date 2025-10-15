google/alioth

commit cff7f79994b6398bad30f74e49d5a40467479b4a

feat(vmnet): add bindings for vmnet framework
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/sys/macos/vmnet.rs
@@ -1,0 +1,121 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use libc::{c_char, c_void, iovec};
+
+use crate::c_enum;
+use crate::sys::block::Block;
+use crate::sys::dispatch::DispatchQueue;
+use crate::sys::xpc::XpcObject;
+
+c_enum! {
+    pub struct VmnetReturn(u32);
+    {
+        SUCCESS = 1000;
+        FAILURE = 1001;
+        MEM_FAILURE = 1002;
+        INVALID_ARGUMENT = 1003;
+        SETUP_INCOMPLETE = 1004;
+        INVALID_ACCESS = 1005;
+        PACKET_TOO_BIG = 1006;
+        BUFFER_EXHAUSTED = 1007;
+        TOO_MANY_PACKETS = 1008;
+        SHARING_SERVICE_BUSY = 1009;
+        NOT_AUTHORIZED = 1010;
+    }
+}
+
+c_enum! {
+    pub struct OperationMode(u32);
+    {
+        HOST = 1000;
+        SHARED = 1001;
+        BRIDGED = 1002;
+    }
+}
+
+c_enum! {
+    pub struct InterfaceEvent(u32);
+    {
+        PACKETS_AVAILABLE = 1 << 0;
+    }
+}
+
+#[repr(transparent)]
+pub struct VmnetInterface(c_void);
+
+#[repr(transparent)]
+pub struct VmnetNetworkConfiguration(c_void);
+
+pub type VmnetInterfaceCompletionHandler =
+    Block<extern "C" fn(*mut c_void, VmnetReturn, *const XpcObject)>;
+
+pub type VmnetInterfaceEventCallback =
+    Block<extern "C" fn(*mut c_void, InterfaceEvent, *const XpcObject)>;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct VmPktDesc {
+    pub vm_pkt_size: usize,
+    pub vm_pkt_iov: *mut iovec,
+    pub vm_pkt_iovcnt: u32,
+    pub vm_flags: u32,
+}
+
+#[link(name = "vmnet", kind = "framework")]
+unsafe extern "C" {
+    pub static vmnet_operation_mode_key: *const c_char;
+    pub static vmnet_interface_id_key: *const c_char;
+    pub static vmnet_mac_address_key: *const c_char;
+    pub static vmnet_mtu_key: *const c_char;
+    pub static vmnet_max_packet_size_key: *const c_char;
+    pub static vmnet_allocate_mac_address_key: *const c_char;
+    pub static vmnet_enable_isolation_key: *const c_char;
+
+    pub fn vmnet_start_interface(
+        interface_desc: *const XpcObject,
+        queue: *const DispatchQueue,
+        handler: *const VmnetInterfaceCompletionHandler,
+    ) -> *mut VmnetInterface;
+
+    pub fn vmnet_stop_interface(
+        interface: *mut VmnetInterface,
+        queue: *const DispatchQueue,
+        handler: *const VmnetInterfaceCompletionHandler,
+    );
+
+    pub fn vmnet_interface_set_event_callback(
+        interface: *mut VmnetInterface,
+        event_mask: InterfaceEvent,
+        queue: *const DispatchQueue,
+        callback: *const VmnetInterfaceEventCallback,
+    ) -> VmnetReturn;
+
+    pub fn vmnet_read(
+        interface: *mut VmnetInterface,
+        packets: *mut VmPktDesc,
+        pktcnt: *mut i32,
+    ) -> VmnetReturn;
+
+    pub fn vmnet_write(
+        interface: *mut VmnetInterface,
+        packets: *mut VmPktDesc,
+        pktcnt: *mut i32,
+    ) -> VmnetReturn;
+
+    pub fn vmnet_network_configuration_create(
+        mode: OperationMode,
+        status: *mut VmnetReturn,
+    ) -> *mut VmnetNetworkConfiguration;
+}

alioth/src/sys/macos/block.rs
@@ -1,0 +1,45 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! https://clang.llvm.org/docs/Block-ABI-Apple.html
+
+use bitflags::bitflags;
+use libc::{c_int, c_ulong, c_void};
+
+#[repr(C)]
+pub struct BlockDescriptor {
+    pub reserved: c_ulong,
+    pub size: c_ulong,
+}
+
+bitflags! {
+    #[repr(transparent)]
+    #[derive(Debug, Clone, Default)]
+    pub struct BlockFlag: c_int {
+        const HAS_STRET = 1 << 29;
+    }
+}
+
+#[repr(C)]
+pub struct Block<F> {
+    pub isa: *const c_void,
+    pub flags: BlockFlag,
+    pub reserved: c_int,
+    pub invoke: F,
+    pub descriptor: *const BlockDescriptor,
+}
+
+unsafe extern "C" {
+    pub static _NSConcreteStackBlock: *const c_void;
+}

alioth/src/sys/macos/dispatch.rs
@@ -12,14 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(target_os = "linux")]
-#[path = "linux/linux.rs"]
-mod linux;
-#[cfg(target_os = "macos")]
-#[path = "macos/macos.rs"]
-mod macos;
+use libc::{c_char, c_void};
 
-#[cfg(target_os = "linux")]
-pub use linux::*;
-#[cfg(target_os = "macos")]
-pub use macos::*;
+#[repr(transparent)]
+pub struct DispatchQueue(c_void);
+
+unsafe extern "C" {
+    pub fn dispatch_queue_create(name: *const c_char, attr: *const c_void) -> *mut DispatchQueue;
+}

alioth/src/sys/macos/macos.rs
@@ -12,14 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(target_os = "linux")]
-#[path = "linux/linux.rs"]
-mod linux;
-#[cfg(target_os = "macos")]
-#[path = "macos/macos.rs"]
-mod macos;
-
-#[cfg(target_os = "linux")]
-pub use linux::*;
-#[cfg(target_os = "macos")]
-pub use macos::*;
+pub mod block;
+pub mod dispatch;
+pub mod vmnet;
+pub mod xpc;

alioth/src/sys/macos/xpc.rs
@@ -1,0 +1,37 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[cfg(test)]
+#[path = "xpc_test.rs"]
+mod tests;
+
+use libc::{c_char, c_void};
+
+#[repr(transparent)]
+pub struct XpcObject(c_void);
+
+unsafe extern "C" {
+    pub fn xpc_dictionary_create(
+        keys: *const *const c_char,
+        values: *const *const XpcObject,
+        count: usize,
+    ) -> *mut XpcObject;
+    pub fn xpc_release(object: *mut XpcObject);
+
+    pub fn xpc_uint64_create(value: u64) -> *mut XpcObject;
+    pub fn xpc_bool_create(value: bool) -> *mut XpcObject;
+
+    pub fn xpc_dictionary_get_uint64(xdict: *const XpcObject, key: *const c_char) -> u64;
+    pub fn xpc_dictionary_get_string(xdict: *const XpcObject, key: *const c_char) -> *const c_char;
+}

alioth/src/sys/macos/xpc_test.rs
@@ -12,14 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(target_os = "linux")]
-#[path = "linux/linux.rs"]
-mod linux;
-#[cfg(target_os = "macos")]
-#[path = "macos/macos.rs"]
-mod macos;
+use super::{
+    XpcObject, xpc_dictionary_create, xpc_dictionary_get_uint64, xpc_release, xpc_uint64_create,
+};
 
-#[cfg(target_os = "linux")]
-pub use linux::*;
-#[cfg(target_os = "macos")]
-pub use macos::*;
+#[test]
+fn test_xpc_create() {
+    let key = c"key".as_ptr();
+    let val: *mut XpcObject;
+    let dict: *mut XpcObject;
+    let num;
+    unsafe {
+        val = xpc_uint64_create(123);
+        dict = xpc_dictionary_create(&key, &(val as *const XpcObject), 1);
+        num = xpc_dictionary_get_uint64(dict, key);
+    }
+    assert_eq!(num, 123);
+    unsafe {
+        xpc_release(val);
+        xpc_release(dict);
+    }
+}

commit 107920ab371d3a17f9d68997012d6a710c8ae2cb

fix(vmnet): fix bindings for vmnet_stop_interface
Fixes: cff7f79994b6 ("feat(vmnet): add bindings for vmnet framework")
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/sys/macos/vmnet.rs
@@ -58,8 +58,10 @@ pub struct VmnetInterface(c_void);
 #[repr(transparent)]
 pub struct VmnetNetworkConfiguration(c_void);
 
-pub type VmnetInterfaceCompletionHandler =
+pub type VmnetStartInterfaceCompletionHandler =
     Block<extern "C" fn(*mut c_void, VmnetReturn, *const XpcObject)>;
+
+pub type VmnetInterfaceCompletionHandler = Block<extern "C" fn(*mut c_void, VmnetReturn)>;
 
 pub type VmnetInterfaceEventCallback =
     Block<extern "C" fn(*mut c_void, InterfaceEvent, *const XpcObject)>;
@@ -86,14 +88,14 @@ unsafe extern "C" {
     pub fn vmnet_start_interface(
         interface_desc: *const XpcObject,
         queue: *const DispatchQueue,
-        handler: *const VmnetInterfaceCompletionHandler,
+        handler: *const VmnetStartInterfaceCompletionHandler,
     ) -> *mut VmnetInterface;
 
     pub fn vmnet_stop_interface(
         interface: *mut VmnetInterface,
         queue: *const DispatchQueue,
         handler: *const VmnetInterfaceCompletionHandler,
-    );
+    ) -> VmnetReturn;
 
     pub fn vmnet_interface_set_event_callback(
         interface: *mut VmnetInterface,

alioth/src/virtio/dev/net/vmnet.rs
@@ -35,8 +35,8 @@ use crate::sync::notifier::Notifier;
 use crate::sys::block::{_NSConcreteStackBlock, BlockDescriptor, BlockFlag};
 use crate::sys::dispatch::{DispatchQueue, dispatch_queue_create};
 use crate::sys::vmnet::{
-    InterfaceEvent, OperationMode, VmPktDesc, VmnetInterface, VmnetInterfaceCompletionHandler,
-    VmnetInterfaceEventCallback, VmnetReturn, vmnet_allocate_mac_address_key,
+    InterfaceEvent, OperationMode, VmPktDesc, VmnetInterface, VmnetInterfaceEventCallback,
+    VmnetReturn, VmnetStartInterfaceCompletionHandler, vmnet_allocate_mac_address_key,
     vmnet_enable_isolation_key, vmnet_interface_set_event_callback, vmnet_mac_address_key,
     vmnet_mtu_key, vmnet_operation_mode_key, vmnet_read, vmnet_start_interface, vmnet_write,
 };
@@ -96,7 +96,7 @@ impl Net {
 
         #[repr(C)]
         struct HandlerBlock {
-            block: VmnetInterfaceCompletionHandler,
+            block: VmnetStartInterfaceCompletionHandler,
             sender: *const Sender<Result<NetConfig>>,
         }
 
@@ -144,7 +144,7 @@ impl Net {
             size: size_of::<HandlerBlock>() as _,
         };
         let handler = HandlerBlock {
-            block: VmnetInterfaceCompletionHandler {
+            block: VmnetStartInterfaceCompletionHandler {
                 isa: unsafe { _NSConcreteStackBlock },
                 flags: BlockFlag::HAS_STRET,
                 reserved: 0,