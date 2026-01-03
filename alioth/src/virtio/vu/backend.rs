google/alioth

commit 128153e1e0177859d8916b2de1d67913aec6a6de

fix(virtio): extend virtio feature to 128 bits
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/vu/backend.rs
@@ -253,7 +253,7 @@ impl VuBackend {
         }
 
         Ok(StartParam {
-            feature: self.init.drv_feat,
+            feature: self.init.drv_feat as u128,
             irq_sender: Arc::new(irq_sender),
             ioeventfds: Some(ioeventfds.into()),
         })

alioth/src/utils/utils.rs
@@ -71,14 +71,6 @@ where
     data.into_iter().fold(0u8, |accu, e| accu.wrapping_add(*e))
 }
 
-pub fn get_low32(num: u64) -> u32 {
-    num as u32
-}
-
-pub fn get_high32(num: u64) -> u32 {
-    (num >> 32) as u32
-}
-
 pub fn get_atomic_low32(num: &AtomicU64) -> u32 {
     num.load(Ordering::Acquire) as u32
 }

alioth/src/virtio/dev/balloon.rs
@@ -84,7 +84,7 @@ impl Mmio for BalloonConfigMmio {
 
 bitflags! {
     #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub struct BalloonFeature: u64 {
+    pub struct BalloonFeature: u128 {
         const MUST_TELL_HOST = 1 << 0;
         const STATS_VQ = 1 << 1;
         const DEFLATE_ON_OOM = 1 << 2;
@@ -221,7 +221,7 @@ impl Virtio for Balloon {
         self.config.clone()
     }
 
-    fn feature(&self) -> u64 {
+    fn feature(&self) -> u128 {
         FEATURE_BUILT_IN | self.feature.bits()
     }
 }
@@ -229,7 +229,7 @@ impl Virtio for Balloon {
 impl VirtioMio for Balloon {
     fn activate<'a, 'm, Q, S, E>(
         &mut self,
-        feature: u64,
+        feature: u128,
         _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
     ) -> Result<()>
     where

alioth/src/virtio/dev/blk.rs
@@ -86,7 +86,7 @@ const SECTOR_SIZE: usize = 1 << 9;
 
 bitflags! {
     #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub struct BlockFeature: u64 {
+    pub struct BlockFeature: u128 {
         const SIZE_MAX = 1 << 1;
         const SEG_MAX = 1 << 2;
         const GEOMETRY = 1 << 4;
@@ -293,7 +293,7 @@ impl Virtio for Block {
         self.config.clone()
     }
 
-    fn feature(&self) -> u64 {
+    fn feature(&self) -> u128 {
         self.feature.bits() | FEATURE_BUILT_IN
     }
 
@@ -320,7 +320,7 @@ impl VirtioMio for Block {
 
     fn activate<'a, 'm, Q, S, E>(
         &mut self,
-        _feature: u64,
+        _feature: u128,
         _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
     ) -> Result<()>
     where
@@ -415,7 +415,7 @@ impl VirtioMio for Block {
 impl VirtioIoUring for Block {
     fn activate<'a, 'm, Q, S, E>(
         &mut self,
-        _feature: u64,
+        _feature: u128,
         _ring: &mut ActiveIoUring<'a, 'm, Q, S, E>,
     ) -> Result<()>
     where

alioth/src/virtio/dev/dev.rs
@@ -26,7 +26,7 @@ pub mod vsock;
 
 use std::fmt::Debug;
 use std::sync::Arc;
-use std::sync::atomic::{AtomicU8, AtomicU16, AtomicU64};
+use std::sync::atomic::{AtomicU8, AtomicU16, AtomicU32};
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread::JoinHandle;
 
@@ -46,13 +46,13 @@ use crate::virtio::{DeviceId, IrqSender, Result, VirtioFeature, error};
 
 pub trait Virtio: Debug + Send + Sync + 'static {
     type Config: Mmio;
-    type Feature: Flags<Bits = u64> + Debug;
+    type Feature: Flags<Bits = u128> + Debug;
 
     fn name(&self) -> &str;
     fn id(&self) -> DeviceId;
     fn num_queues(&self) -> u16;
     fn config(&self) -> Arc<Self::Config>;
-    fn feature(&self) -> u64;
+    fn feature(&self) -> u128;
     fn spawn_worker<S: IrqSender, E: IoeventFd>(
         self,
         event_rx: Receiver<WakeEvent<S, E>>,
@@ -77,8 +77,8 @@ pub trait Virtio: Debug + Send + Sync + 'static {
 
 #[derive(Debug, Default)]
 pub struct Register {
-    pub device_feature: u64,
-    pub driver_feature: AtomicU64,
+    pub device_feature: [u32; 4],
+    pub driver_feature: [AtomicU32; 4],
     pub device_feature_sel: AtomicU8,
     pub driver_feature_sel: AtomicU8,
     pub queue_sel: AtomicU16,
@@ -93,7 +93,7 @@ where
     S: IrqSender,
     E: IoeventFd,
 {
-    pub(crate) feature: u64,
+    pub(crate) feature: u128,
     pub(crate) irq_sender: Arc<S>,
     pub(crate) ioeventfds: Option<Arc<[E]>>,
 }
@@ -144,7 +144,7 @@ where
     pub name: Arc<str>,
     pub id: DeviceId,
     pub device_config: Arc<dyn Mmio>,
-    pub device_feature: u64,
+    pub device_feature: u128,
     pub queue_regs: Arc<[Queue]>,
     pub shared_mem_regions: Option<Arc<MemRegion>>,
     pub waker: Arc<Waker>,

alioth/src/virtio/dev/entropy.rs
@@ -54,7 +54,7 @@ impl Mmio for EntropyConfig {
 
 bitflags! {
     #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub struct EntropyFeature: u64 { }
+    pub struct EntropyFeature: u128 { }
 }
 
 #[derive(Debug)]
@@ -111,7 +111,7 @@ impl Virtio for Entropy {
         self.config.clone()
     }
 
-    fn feature(&self) -> u64 {
+    fn feature(&self) -> u128 {
         FEATURE_BUILT_IN
     }
 }
@@ -119,7 +119,7 @@ impl Virtio for Entropy {
 impl VirtioMio for Entropy {
     fn activate<'a, 'm, Q, S, E>(
         &mut self,
-        _feature: u64,
+        _feature: u128,
         _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
     ) -> Result<()>
     where

alioth/src/virtio/dev/fs/fs.rs
@@ -107,7 +107,7 @@ impl_mmio_for_zerocopy!(FsConfig);
 
 bitflags! {
     #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub struct FsFeature: u64 {
+    pub struct FsFeature: u128 {
         const NOTIFICATION = 1 << 0;
     }
 }
@@ -340,7 +340,7 @@ where
 {
     fn activate<'a, 'm, Q, S, E>(
         &mut self,
-        feature: u64,
+        feature: u128,
         _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
     ) -> Result<()>
     where
@@ -408,7 +408,7 @@ where
         &self.name
     }
 
-    fn feature(&self) -> u64 {
+    fn feature(&self) -> u128 {
         self.feature.bits() | FEATURE_BUILT_IN
     }
 

alioth/src/virtio/dev/fs/vu.rs
@@ -142,7 +142,7 @@ impl Virtio for VuFs {
         self.config.clone()
     }
 
-    fn feature(&self) -> u64 {
+    fn feature(&self) -> u128 {
         self.frontend.feature()
     }
 
@@ -183,7 +183,7 @@ impl Virtio for VuFs {
 impl VirtioMio for VuFs {
     fn activate<'a, 'm, Q, S, E>(
         &mut self,
-        feature: u64,
+        feature: u128,
         active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
     ) -> Result<()>
     where

commit d23d9ec02ca434cbbb4c8a0b56f02471571b66ad

fix(vu): correctly handle 128-bit device features
The vhost-user protocol requires 64-bit device features, but the virtio
spec allows 128. This commit truncates features to 64 bits before
sending, fixing a bug where all 128 bits were sent, causing feature
negotiation to fail.

Fixes: 128153e1e017 ("fix(virtio): extend virtio feature to 128 bits")
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/vu/backend.rs
@@ -283,7 +283,7 @@ impl VuBackend {
             }
             (VuFrontMsg::GET_FEATURES, 0) => {
                 let feature = self.dev.device_feature | VirtioFeature::VHOST_PROTOCOL.bits();
-                self.session.reply(req, &feature, &[])?;
+                self.session.reply(req, &(feature as u64), &[])?;
                 msg.flag.set_need_reply(false);
                 log::debug!("{name}: get device feature: {feature:#x}");
             }