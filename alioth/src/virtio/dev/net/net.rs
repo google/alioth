google/alioth

commit e2f11df3475af8bc162a40d439bfc198b17553b6

fix(virtio): devices return the full feature sets
Previously, each device only returns the device specific features
and VirtioDevice::new() adds the general virtio feature bits.
This causes troubles for supporting vhost-user backends, which may
implement a different set of general virtio feature bits.

This commit lets individual devices return the full feature sets.
The general virtio feature bits implemented by the module virtio
is moved to `FEATURE_BUILT_IN`, which implicitly turns on the
`EVENT_IDX` for the entropy and block device.

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/dev/net/net.rs
@@ -37,7 +37,7 @@ use crate::net::MacAddr;
 use crate::virtio::dev::{DevParam, DeviceId, Result, Virtio};
 use crate::virtio::queue::handlers::{queue_to_writer, reader_to_queue};
 use crate::virtio::queue::VirtQueue;
-use crate::virtio::{IrqSender, VirtioFeature};
+use crate::virtio::{IrqSender, FEATURE_BUILT_IN};
 
 pub mod tap;
 
@@ -188,7 +188,7 @@ impl Virtio for Net {
     }
 
     fn feature(&self) -> u64 {
-        self.feature.bits() | VirtioFeature::EVENT_IDX.bits()
+        self.feature.bits() | FEATURE_BUILT_IN
     }
 
     fn activate(&mut self, registry: &Registry, feature: u64, _memory: &RamBus) -> Result<()> {

alioth/src/virtio/dev/blk.rs
@@ -28,7 +28,7 @@ use crate::mem::mapped::RamBus;
 use crate::virtio::dev::{DevParam, Virtio};
 use crate::virtio::queue::handlers::handle_desc;
 use crate::virtio::queue::{Descriptor, VirtQueue};
-use crate::virtio::{DeviceId, IrqSender, Result};
+use crate::virtio::{DeviceId, IrqSender, Result, FEATURE_BUILT_IN};
 
 pub const VIRTIO_BLK_T_IN: u32 = 0;
 pub const VIRTIO_BLK_T_OUT: u32 = 1;
@@ -260,7 +260,7 @@ impl Virtio for Block {
     }
 
     fn feature(&self) -> u64 {
-        self.feature.bits()
+        self.feature.bits() | FEATURE_BUILT_IN
     }
 
     fn activate(&mut self, _registry: &Registry, _feature: u64, _memory: &RamBus) -> Result<()> {

alioth/src/virtio/dev/dev.rs
@@ -159,7 +159,7 @@ where
         let poll = Poll::new()?;
         let device_config = dev.config();
         let reg = Arc::new(Register {
-            device_feature: dev.feature() | VirtioFeature::SUPPORTED.bits(),
+            device_feature: dev.feature(),
             ..Default::default()
         });
         let num_queues = dev.num_queues();

alioth/src/virtio/dev/entropy.rs
@@ -28,7 +28,7 @@ use crate::mem::mapped::RamBus;
 use crate::virtio::dev::{DevParam, DeviceId, Virtio};
 use crate::virtio::queue::handlers::reader_to_queue;
 use crate::virtio::queue::VirtQueue;
-use crate::virtio::{IrqSender, Result};
+use crate::virtio::{IrqSender, Result, FEATURE_BUILT_IN};
 
 #[derive(Debug, Clone)]
 pub struct EntropyConfig;
@@ -109,7 +109,7 @@ impl Virtio for Entropy {
     }
 
     fn feature(&self) -> u64 {
-        0
+        FEATURE_BUILT_IN
     }
 
     fn activate(&mut self, _registry: &Registry, _feature: u64, _memory: &RamBus) -> Result<()> {

alioth/src/virtio/virtio.rs
@@ -53,9 +53,12 @@ bitflags! {
         const VERSION_1 = 1 << 32;
         const ACCESS_PLATFORM = 1 << 33;
         const RING_PACKED = 1 << 34;
-        const SUPPORTED = Self::VERSION_1.bits() | Self::ACCESS_PLATFORM.bits();
     }
 }
+
+const FEATURE_BUILT_IN: u64 = VirtioFeature::EVENT_IDX.bits()
+    | VirtioFeature::VERSION_1.bits()
+    | VirtioFeature::ACCESS_PLATFORM.bits();
 
 #[derive(Debug, Clone, Copy)]
 pub enum DeviceId {

commit 934a9722373044f167d01182a99c7d426467b789

fix(net): remove INDIRECT_DESC from feature set
INDIRECT_DESC is a virtio queue feature.

Fixes: e2f11df3475a ("fix(virtio): devices return the full feature
sets")
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/dev/net/net.rs
@@ -112,7 +112,6 @@ bitflags! {
         const RSC_EXT = 1 << 61;
         const STANDBY = 1 << 62;
         const SPEED_DUPLEX = 1 << 63;
-        const INDIRECT_DESC = 1 << 28;
     }
 }
 