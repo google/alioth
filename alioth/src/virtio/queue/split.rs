google/alioth

commit 2d53e34b0a0a569481b78157f3ed63af6ac7c032

refactor(virtio): move split/packed common fields to Queue
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/queue/split.rs
@@ -16,6 +16,7 @@
 #[cfg(test)]
 mod tests;
 
+use std::marker::PhantomData;
 use std::mem::size_of;
 use std::sync::atomic::{Ordering, fence};
 
@@ -24,7 +25,6 @@ use bitflags::bitflags;
 use zerocopy::{FromBytes, Immutable, IntoBytes};
 
 use crate::mem::mapped::Ram;
-use crate::virtio::queue::private::VirtQueuePrivate;
 use crate::virtio::queue::{DescChain, DescFlag, QueueReg, VirtQueue};
 use crate::virtio::{Result, error};
 
@@ -73,9 +73,7 @@ pub struct UsedElem {
 }
 
 #[derive(Debug)]
-pub struct SplitQueue<'r, 'm> {
-    reg: &'r QueueReg,
-    ram: &'m Ram,
+pub struct SplitQueue<'m> {
     size: u16,
     avail_hdr: *mut AvailHeader,
     avail_ring: *mut u16,
@@ -83,13 +81,11 @@ pub struct SplitQueue<'r, 'm> {
     used_hdr: *mut UsedHeader,
     used_ring: *mut UsedElem,
     avail_event: Option<*mut u16>,
-    used_index: u16,
     desc: *mut Desc,
+    _phantom: PhantomData<&'m ()>,
 }
 
-type DescIov = (Vec<(u64, u64)>, Vec<(u64, u64)>);
-
-impl<'m> SplitQueue<'_, 'm> {
+impl SplitQueue<'_> {
     pub fn avail_index(&self) -> u16 {
         unsafe { &*self.avail_hdr }.idx
     }
@@ -127,61 +123,10 @@ impl<'m> SplitQueue<'_, 'm> {
             error::InvalidDescriptor { id }.fail()
         }
     }
-
-    fn get_indirect(
-        &self,
-        addr: u64,
-        readable: &mut Vec<(u64, u64)>,
-        writeable: &mut Vec<(u64, u64)>,
-    ) -> Result<()> {
-        let mut id = 0;
-        loop {
-            let desc: Desc = self.ram.read_t(addr + id * size_of::<Desc>() as u64)?;
-            let flag = DescFlag::from_bits_retain(desc.flag);
-            assert!(!flag.contains(DescFlag::INDIRECT));
-            if flag.contains(DescFlag::WRITE) {
-                writeable.push((desc.addr, desc.len as u64));
-            } else {
-                readable.push((desc.addr, desc.len as u64));
-            }
-            if flag.contains(DescFlag::NEXT) {
-                id = desc.next as u64;
-            } else {
-                return Ok(());
-            }
-        }
-    }
-
-    pub fn get_desc_iov(&self, mut id: u16) -> Result<DescIov> {
-        let mut readable = Vec::new();
-        let mut writeable = Vec::new();
-        loop {
-            let desc = self.get_desc(id)?;
-            let flag = DescFlag::from_bits_retain(desc.flag);
-            if flag.contains(DescFlag::INDIRECT) {
-                assert_eq!(desc.len & 0xf, 0);
-                self.get_indirect(desc.addr, &mut readable, &mut writeable)?;
-            } else if flag.contains(DescFlag::WRITE) {
-                writeable.push((desc.addr, desc.len as u64));
-            } else {
-                readable.push((desc.addr, desc.len as u64));
-            }
-            if flag.contains(DescFlag::NEXT) {
-                id = desc.next;
-            } else {
-                break;
-            }
-        }
-        Ok((readable, writeable))
-    }
 }
 
-impl<'r, 'm> SplitQueue<'r, 'm> {
-    pub fn new(
-        reg: &'r QueueReg,
-        ram: &'m Ram,
-        event_idx: bool,
-    ) -> Result<Option<SplitQueue<'r, 'm>>> {
+impl<'m> SplitQueue<'m> {
+    pub fn new(reg: &QueueReg, ram: &'m Ram, event_idx: bool) -> Result<Option<SplitQueue<'m>>> {
         if !reg.enabled.load(Ordering::Acquire) {
             return Ok(None);
         }
@@ -199,13 +144,10 @@ impl<'r, 'm> SplitQueue<'r, 'm> {
             used_event = Some(ram.get_ptr(used_event_gpa)?);
         }
         let used_hdr = ram.get_ptr::<UsedHeader>(used)?;
-        let used_index = unsafe { &*used_hdr }.idx;
         let avail_ring_gpa = avail + size_of::<AvailHeader>() as u64;
         let used_ring_gpa = used + size_of::<UsedHeader>() as u64;
         let desc = reg.desc.load(Ordering::Acquire);
         Ok(Some(SplitQueue {
-            reg,
-            ram,
             size: size as u16,
             avail_hdr: ram.get_ptr(avail)?,
             avail_ring: ram.get_ptr(avail_ring_gpa)?,
@@ -213,13 +155,13 @@ impl<'r, 'm> SplitQueue<'r, 'm> {
             used_hdr,
             used_ring: ram.get_ptr(used_ring_gpa)?,
             avail_event,
-            used_index,
             desc: ram.get_ptr(desc)?,
+            _phantom: PhantomData,
         }))
     }
 }
 
-impl<'m> VirtQueuePrivate<'m> for SplitQueue<'_, 'm> {
+impl<'m> VirtQueue<'m> for SplitQueue<'m> {
     type Index = u16;
 
     const INIT_INDEX: u16 = 0;
@@ -229,34 +171,64 @@ impl<'m> VirtQueuePrivate<'m> for SplitQueue<'_, 'm> {
         index < avail_index || index - avail_index >= !(self.size - 1)
     }
 
-    fn get_desc_chain(&self, index: u16) -> Result<Option<DescChain<'m>>> {
+    fn get_avail(&self, index: Self::Index, ram: &'m Ram) -> Result<Option<DescChain<'m>>> {
         if !self.desc_avail(index) {
             return Ok(None);
         }
+        let mut readable = Vec::new();
+        let mut writable = Vec::new();
         let wrapped_index = index & (self.size - 1);
-        let desc_id = unsafe { *self.avail_ring.offset(wrapped_index as isize) };
-        let (readable, writable) = self.get_desc_iov(desc_id)?;
-        let readable = self.ram.translate_iov(&readable)?;
-        let writable = self.ram.translate_iov_mut(&writable)?;
+        let head_id = unsafe { *self.avail_ring.offset(wrapped_index as isize) };
+        let mut id = head_id;
+        loop {
+            let desc = self.get_desc(id)?;
+            let flag = DescFlag::from_bits_retain(desc.flag);
+            if flag.contains(DescFlag::INDIRECT) {
+                let mut id = 0;
+                loop {
+                    let addr = desc.addr + id as u64 * size_of::<Desc>() as u64;
+                    let desc: Desc = ram.read_t(addr)?;
+                    let flag = DescFlag::from_bits_retain(desc.flag);
+                    assert!(!flag.contains(DescFlag::INDIRECT));
+                    if flag.contains(DescFlag::WRITE) {
+                        writable.push((desc.addr, desc.len as u64));
+                    } else {
+                        readable.push((desc.addr, desc.len as u64));
+                    }
+                    if flag.contains(DescFlag::NEXT) {
+                        id = desc.next;
+                    } else {
+                        break;
+                    }
+                }
+            } else if flag.contains(DescFlag::WRITE) {
+                writable.push((desc.addr, desc.len as u64));
+            } else {
+                readable.push((desc.addr, desc.len as u64));
+            }
+            if flag.contains(DescFlag::NEXT) {
+                id = desc.next;
+            } else {
+                break;
+            }
+        }
+        let readable = ram.translate_iov(&readable)?;
+        let writable = ram.translate_iov_mut(&writable)?;
         Ok(Some(DescChain {
-            id: desc_id,
-            index,
+            id: head_id,
             delta: 1,
             readable,
             writable,
         }))
     }
 
-    fn push_used(&mut self, chain: DescChain, len: u32) {
-        let used_elem = UsedElem {
-            id: chain.id as u32,
-            len,
-        };
-        let wrapped_index = self.used_index & (self.size - 1);
+    fn set_used(&self, index: Self::Index, id: u16, len: u32) {
+        let used_elem = UsedElem { id: id as u32, len };
+        log::info!("used_elem: {used_elem:x?}");
+        let wrapped_index = index & (self.size - 1);
         unsafe { *self.used_ring.offset(wrapped_index as isize) = used_elem };
         fence(Ordering::SeqCst);
-        self.used_index = self.used_index.wrapping_add(1);
-        self.set_used_index(self.used_index);
+        self.set_used_index(index.wrapping_add(1));
     }
 
     fn enable_notification(&self, enabled: bool) {
@@ -281,20 +253,14 @@ impl<'m> VirtQueuePrivate<'m> for SplitQueue<'_, 'm> {
         }
     }
 
-    fn interrupt_enabled(&self, _: u16) -> bool {
+    fn interrupt_enabled(&self, index: Self::Index, _: u16) -> bool {
         match self.used_event() {
-            Some(used_event) => used_event == self.used_index.wrapping_sub(1),
+            Some(used_event) => used_event == index.wrapping_sub(1),
             None => self.flag_interrupt_enabled(),
         }
     }
 
-    fn next_index(&self, chain: &DescChain) -> u16 {
-        chain.index.wrapping_add(1)
-    }
-}
-
-impl<'m> VirtQueue<'m> for SplitQueue<'_, 'm> {
-    fn reg(&self) -> &QueueReg {
-        self.reg
+    fn index_add(&self, index: Self::Index, _: u16) -> Self::Index {
+        index.wrapping_add(1)
     }
 }

alioth/src/virtio/dev/balloon.rs
@@ -227,10 +227,10 @@ impl Virtio for Balloon {
 }
 
 impl VirtioMio for Balloon {
-    fn activate<'a, 'm, Q, S, E>(
+    fn activate<'m, Q, S, E>(
         &mut self,
         feature: u128,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -255,10 +255,10 @@ impl VirtioMio for Balloon {
         Ok(())
     }
 
-    fn handle_queue<'a, 'm, Q, S, E>(
+    fn handle_queue<'m, Q, S, E>(
         &mut self,
         index: u16,
-        active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -301,7 +301,7 @@ impl VirtioMio for Balloon {
     fn handle_event<'a, 'm, Q, S, E>(
         &mut self,
         _event: &Event,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,

alioth/src/virtio/dev/blk.rs
@@ -318,10 +318,10 @@ impl Virtio for Block {
 impl VirtioMio for Block {
     fn reset(&mut self, _registry: &Registry) {}
 
-    fn activate<'a, 'm, Q, S, E>(
+    fn activate<'m, Q, S, E>(
         &mut self,
         _feature: u128,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -334,7 +334,7 @@ impl VirtioMio for Block {
     fn handle_event<'a, 'm, Q, S, E>(
         &mut self,
         _event: &Event,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -344,10 +344,10 @@ impl VirtioMio for Block {
         Ok(())
     }
 
-    fn handle_queue<'a, 'm, Q, S, E>(
+    fn handle_queue<'m, Q, S, E>(
         &mut self,
         index: u16,
-        active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -413,10 +413,10 @@ impl VirtioMio for Block {
 
 #[cfg(target_os = "linux")]
 impl VirtioIoUring for Block {
-    fn activate<'a, 'm, Q, S, E>(
+    fn activate<'m, Q, S, E>(
         &mut self,
         _feature: u128,
-        _ring: &mut ActiveIoUring<'a, 'm, Q, S, E>,
+        _ring: &mut ActiveIoUring<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         S: IrqSender,

alioth/src/virtio/dev/dev.rs
@@ -237,7 +237,7 @@ pub trait Backend<D: Virtio>: Send + 'static {
         &mut self,
         memory: &'m Ram,
         context: &mut Context<D, S, E>,
-        queues: &mut [Option<Queue<'m, Q>>],
+        queues: &mut [Option<Queue<'_, 'm, Q>>],
         param: &StartParam<S, E>,
     ) -> Result<()>
     where
@@ -371,7 +371,7 @@ where
 
     fn event_loop<'m, Q>(
         &mut self,
-        queues: &mut [Option<Queue<'m, Q>>],
+        queues: &mut [Option<Queue<'_, 'm, Q>>],
         ram: &'m Ram,
         param: &StartParam<S, E>,
     ) -> Result<()>
@@ -404,7 +404,7 @@ where
                 let Some(split_queue) = PackedQueue::new(reg, &ram, event_idx)? else {
                     return Ok(None);
                 };
-                Ok(Some(Queue::new(split_queue)))
+                Ok(Some(Queue::new(split_queue, reg, &ram)))
             };
             let queues: Result<Box<_>> = queue_regs.iter().map(new_queue).collect();
             self.event_loop(&mut (queues?), &ram, &param)?;
@@ -413,7 +413,7 @@ where
                 let Some(split_queue) = SplitQueue::new(reg, &ram, event_idx)? else {
                     return Ok(None);
                 };
-                Ok(Some(Queue::new(split_queue)))
+                Ok(Some(Queue::new(split_queue, reg, &ram)))
             };
             let queues: Result<Box<_>> = queue_regs.iter().map(new_queue).collect();
             self.event_loop(&mut (queues?), &ram, &param)?;

alioth/src/virtio/dev/entropy.rs
@@ -125,10 +125,10 @@ impl Virtio for Entropy {
 }
 
 impl VirtioMio for Entropy {
-    fn activate<'a, 'm, Q, S, E>(
+    fn activate<'m, Q, S, E>(
         &mut self,
         _feature: u128,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -138,10 +138,10 @@ impl VirtioMio for Entropy {
         Ok(())
     }
 
-    fn handle_queue<'a, 'm, Q, S, E>(
+    fn handle_queue<'m, Q, S, E>(
         &mut self,
         index: u16,
-        active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -158,7 +158,7 @@ impl VirtioMio for Entropy {
     fn handle_event<'a, 'm, Q, S, E>(
         &mut self,
         _event: &Event,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,

alioth/src/virtio/dev/entropy_test.rs
@@ -52,7 +52,7 @@ fn entropy_test(fixture_ram_bus: RamBus, fixture_queue: QueueReg) {
     let queues = Arc::new([fixture_queue]);
 
     let q = SplitQueue::new(&queues[0], &*ram, false).unwrap().unwrap();
-    let mut q = Queue::new(q);
+    let mut q = Queue::new(q, &queues[0], &ram);
 
     let buf0_addr = DATA_ADDR;
     let buf1_addr = buf0_addr + (4 << 10);

alioth/src/virtio/dev/fs/fs.rs
@@ -338,10 +338,10 @@ impl<F> VirtioMio for Fs<F>
 where
     F: Fuse + Debug + Send + Sync + 'static,
 {
-    fn activate<'a, 'm, Q, S, E>(
+    fn activate<'m, Q, S, E>(
         &mut self,
         feature: u128,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -355,7 +355,7 @@ where
     fn handle_event<'a, 'm, Q, S, E>(
         &mut self,
         _event: &Event,
-        _active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        _active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -365,10 +365,10 @@ where
         unreachable!()
     }
 
-    fn handle_queue<'a, 'm, Q, S, E>(
+    fn handle_queue<'m, Q, S, E>(
         &mut self,
         index: u16,
-        active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,

alioth/src/virtio/dev/fs/vu.rs
@@ -181,10 +181,10 @@ impl Virtio for VuFs {
 }
 
 impl VirtioMio for VuFs {
-    fn activate<'a, 'm, Q, S, E>(
+    fn activate<'m, Q, S, E>(
         &mut self,
         feature: u128,
-        active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -206,7 +206,7 @@ impl VirtioMio for VuFs {
     fn handle_event<'a, 'm, Q, S, E>(
         &mut self,
         event: &Event,
-        active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,
@@ -302,10 +302,10 @@ impl VirtioMio for VuFs {
         Ok(())
     }
 
-    fn handle_queue<'a, 'm, Q, S, E>(
+    fn handle_queue<'m, Q, S, E>(
         &mut self,
         index: u16,
-        active_mio: &mut ActiveMio<'a, 'm, Q, S, E>,
+        active_mio: &mut ActiveMio<'_, '_, 'm, Q, S, E>,
     ) -> Result<()>
     where
         Q: VirtQueue<'m>,

commit dbc7058f7f39c12999c3ba3063ae97994aff3154

fix(virtio): avoid log spam in split queues
Fixes: 2d53e34b0a0a ("refactor(virtio): move split/packed common fields to Queue")

Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/virtio/queue/split.rs
@@ -220,7 +220,6 @@ impl<'m> VirtQueue<'m> for SplitQueue<'m> {
 
     fn set_used(&self, index: Self::Index, id: u16, len: u32) {
         let used_elem = UsedElem { id: id as u32, len };
-        log::info!("used_elem: {used_elem:x?}");
         let wrapped_index = index & (self.size - 1);
         unsafe { *self.used_ring.offset(wrapped_index as isize) = used_elem };
         fence(Ordering::SeqCst);