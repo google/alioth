google/alioth

commit abc6faa602257b18fc25fcad1f27c94dff2f1ab0

feat(mem): enable device p2p DMA
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/mem/mem.rs
@@ -245,6 +245,8 @@ impl SlotBackend for Arc<IoRegion> {
 pub trait LayoutChanged: Debug + Send + Sync + 'static {
     fn ram_added(&self, gpa: u64, pages: &ArcMemPages) -> Result<()>;
     fn ram_removed(&self, gpa: u64, pages: &ArcMemPages) -> Result<()>;
+    fn dev_mem_added(&self, gpa: u64, pages: &ArcMemPages) -> Result<()>;
+    fn dev_mem_removed(&self, gpa: u64, pages: &ArcMemPages) -> Result<()>;
 }
 
 pub trait LayoutUpdated: Debug + Send + Sync + 'static {
@@ -353,20 +355,26 @@ impl Memory {
         let mut ram_updated = false;
         for range in &region.ranges {
             let gpa = addr + offset;
+            if let MemRange::Ram(r) | MemRange::DevMem(r) = range {
+                self.ram_bus.add(gpa, r.clone())?;
+                self.map_to_vm(gpa, r)?;
+                ram_updated = true;
+            }
             match range {
                 MemRange::Emulated(r) => {
                     let mut mmio_bus = self.mmio_bus.write();
                     mmio_bus.add(gpa, r.clone())?
                 }
                 MemRange::Ram(r) => {
-                    self.map_to_vm(gpa, r)?;
                     for callback in &callbacks.changed {
                         callback.ram_added(gpa, r)?;
                     }
-                    self.ram_bus.add(gpa, r.clone())?;
-                    ram_updated = true;
                 }
-                MemRange::DevMem(r) => self.map_to_vm(gpa, r)?,
+                MemRange::DevMem(r) => {
+                    for callback in &callbacks.changed {
+                        callback.dev_mem_added(gpa, r)?;
+                    }
+                }
                 MemRange::Span(_) => {}
             }
             offset += range.size();
@@ -390,20 +398,26 @@ impl Memory {
         let mut ram_updated = false;
         for range in &region.ranges {
             let gpa = addr + offset;
+            if let MemRange::Ram(r) | MemRange::DevMem(r) = range {
+                self.ram_bus.remove(gpa)?;
+                self.unmap_from_vm(gpa, r)?;
+                ram_updated = true;
+            }
             match range {
                 MemRange::Emulated(_) => {
                     let mut mmio_bus = self.mmio_bus.write();
                     mmio_bus.remove(gpa)?;
                 }
                 MemRange::Ram(r) => {
-                    self.ram_bus.remove(gpa)?;
                     for callback in callbacks.changed.iter().rev() {
                         callback.ram_removed(gpa, r)?;
                     }
-                    self.unmap_from_vm(gpa, r)?;
-                    ram_updated = true;
                 }
-                MemRange::DevMem(r) => self.unmap_from_vm(gpa, r)?,
+                MemRange::DevMem(r) => {
+                    for callback in callbacks.changed.iter().rev() {
+                        callback.dev_mem_removed(gpa, r)?;
+                    }
+                }
                 MemRange::Span(_) => {}
             };
             offset += range.size();
@@ -618,6 +632,14 @@ impl LayoutChanged for MarkPrivateMemory {
     }
 
     fn ram_removed(&self, _: u64, _: &ArcMemPages) -> Result<()> {
+        Ok(())
+    }
+
+    fn dev_mem_added(&self, _: u64, _: &ArcMemPages) -> Result<()> {
+        Ok(())
+    }
+
+    fn dev_mem_removed(&self, _: u64, _: &ArcMemPages) -> Result<()> {
         Ok(())
     }
 }

alioth/src/board/board_amd64/sev.rs
@@ -141,4 +141,12 @@ impl LayoutChanged for EncryptPages {
     fn ram_removed(&self, _: u64, _: &ArcMemPages) -> mem::Result<()> {
         Ok(())
     }
+
+    fn dev_mem_added(&self, _: u64, _: &ArcMemPages) -> mem::Result<()> {
+        Ok(())
+    }
+
+    fn dev_mem_removed(&self, _: u64, _: &ArcMemPages) -> mem::Result<()> {
+        Ok(())
+    }
 }

alioth/src/vfio/container.rs
@@ -119,4 +119,12 @@ impl LayoutChanged for UpdateContainerMapping {
         ret.box_trace(mem::error::ChangeLayout)?;
         Ok(())
     }
+
+    fn dev_mem_added(&self, gpa: u64, pages: &ArcMemPages) -> mem::Result<()> {
+        self.ram_added(gpa, pages)
+    }
+
+    fn dev_mem_removed(&self, gpa: u64, pages: &ArcMemPages) -> mem::Result<()> {
+        self.ram_removed(gpa, pages)
+    }
 }

alioth/src/vfio/iommu.rs
@@ -137,4 +137,13 @@ impl LayoutChanged for UpdateIommuIoas {
         ret.box_trace(mem::error::ChangeLayout)?;
         Ok(())
     }
+
+    fn dev_mem_added(&self, _gpa: u64, _pages: &ArcMemPages) -> mem::Result<()> {
+        // Iommufd does not support mapping device memory into IOAS.
+        Ok(())
+    }
+
+    fn dev_mem_removed(&self, _gpa: u64, _pages: &ArcMemPages) -> mem::Result<()> {
+        Ok(())
+    }
 }

alioth/src/virtio/vu/frontend.rs
@@ -89,6 +89,14 @@ impl LayoutChanged for UpdateVuMem {
         log::trace!("{}: remove memory region: {:x?}", self.name, region.region);
         Ok(())
     }
+
+    fn dev_mem_added(&self, gpa: u64, pages: &ArcMemPages) -> mem::Result<()> {
+        self.ram_added(gpa, pages)
+    }
+
+    fn dev_mem_removed(&self, gpa: u64, pages: &ArcMemPages) -> mem::Result<()> {
+        self.ram_removed(gpa, pages)
+    }
 }
 
 #[derive(Debug)]

commit 1bd5952356ec15cfeba15d65520c6062ec381cce

fix(mem): call dev_mem_added upon new callback registration
Fixes: abc6faa60225 ("feat(mem): enable device p2p DMA")
Signed-off-by: Changyuan Lyu <changyuanl@google.com>

alioth/src/mem/mem.rs
@@ -285,7 +285,8 @@ impl Memory {
                 let gpa = addr + offset;
                 match range {
                     MemRange::Ram(r) => callback.ram_added(gpa, r)?,
-                    MemRange::Span(_) | MemRange::Emulated(_) | MemRange::DevMem(_) => {}
+                    MemRange::DevMem(r) => callback.dev_mem_added(gpa, r)?,
+                    MemRange::Span(_) | MemRange::Emulated(_) => {}
                 }
                 offset += range.size();
             }